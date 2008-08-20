6013b1afdcfe05ce79f0101ddd8468f1e4178ef0
Emmanuel Bernard <emmanuel@hibernate.org>
2008-08-20 14:06:18 +0000

    Wrong vcersion on validator Version.java
    git-svn-id: https://svn.jboss.org/repos/hibernate/validator/trunk@15135 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/validator/Version.java
+++ src/java/org/hibernate/validator/Version.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.1.0.CR1";
+	public static final String VERSION = "3.1.0.CR2";
 	private static Logger log = LoggerFactory.getLogger( Version.class );
 
 	static {
