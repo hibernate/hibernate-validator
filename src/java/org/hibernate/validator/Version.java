ad73cdfbf7f37396516c09ad9ad417b066e9d90a
Hardy Ferentschik <hibernate@ferentschik.de>
2008-09-10 10:53:41 +0000

    Preperation for 3.1.0.GA
    git-svn-id: https://svn.jboss.org/repos/hibernate/validator/trunk@15169 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/validator/Version.java
+++ src/java/org/hibernate/validator/Version.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.1.0.CR2";
+	public static final String VERSION = "3.1.0.GA";
 	private static Logger log = LoggerFactory.getLogger( Version.class );
 
 	static {
