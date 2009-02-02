ba6f02fe778e6cc43071872a2286d1c715121f6e
Emmanuel Bernard <emmanuel@hibernate.org>
2009-02-02 15:37:19 +0000

    BVAL-106 Constraintdescriptor.getConstraintValidatorClasses() should return a List, not an array
    git-svn-id: https://svn.jboss.org/repos/hibernate/validator/trunk@15849 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- validation-api/src/main/java/javax/validation/ConstraintDescriptor.java
+++ validation-api/src/main/java/javax/validation/ConstraintDescriptor.java
@@ -22,60 +22,59 @@ import java.util.Map;
 import java.util.Set;
 import java.util.List;
 
-/**
- * Describes a single constraint and its composing constraints.
- *
- * @author Emmanuel Bernard
- * @author Hardy Ferentschik
- */
-public interface ConstraintDescriptor {
 	/**
-	 * Returns the annotation describing the constraint declaration.
-	 * If a composing constraint, parameter values are reflecting
-	 * the overridden parameters from the main constraint
+	 * Describes a single constraint and its composing constraints.
 	 *
-	 * @return The annotation for this constraint.
+	 * @author Emmanuel Bernard
+	 * @author Hardy Ferentschik
 	 */
-	Annotation getAnnotation();
+	public interface ConstraintDescriptor {
+		/**
+		 * Returns the annotation describing the constraint declaration.
+		 * If a composing constraint, parameter values are reflecting
+		 * the overridden parameters from the main constraint
+		 *
+		 * @return The annotation for this constraint.
+		 */
+		Annotation getAnnotation();
 
-	/**
-	 * @return The groups the constraint is applied on.
-	 */
-	Set<Class<?>> getGroups();
+		/**
+		 * @return The groups the constraint is applied on.
+		 */
+		Set<Class<?>> getGroups();
 
-	/**
-	 * list of the constraint validation implementation classes
-	 * The lsit is immutable
-	 *
-	 * @return list of the constraint validation implementation classes
-	 */
-	List<Class<? extends ConstraintValidator<?,?>>>
-	    getConstraintValidatorClasses();
+		/**
+		 * list of the constraint validation implementation classes
+		 *
+		 * @return list of the constraint validation implementation classes
+		 */
+		List<Class<? extends ConstraintValidator<?,?>>>
+			getConstraintValidatorClasses();
 
-	/**
-	 * Returns a map containing the annotation parameter names as keys and the
-	 * annotation parameter values as value.
-	 * If this constraint is used as part of a composed constraint, parameter
-	 * values are reflecting the overridden parameters from the main constraint.
-	 *
-	 * @return Returns a map containing the annotation paramter names as keys
-	 *         and the annotation parameter values as value.
-	 */
-	Map<String, Object> getParameters();
+		/**
+		 * Returns a map containing the annotation parameter names as keys and the
+		 * annotation parameter values as value.
+		 * If this constraint is used as part of a composed constraint, parameter
+		 * values are reflecting the overridden parameters from the main constraint.
+		 *
+		 * @return Returns a map containing the annotation paramter names as keys
+		 *         and the annotation parameter values as value.
+		 */
+		Map<String, Object> getParameters();
 
-	/**
-	 * Return a set of composing <code>ConstraintDescriptor</code>s where each
-	 * descriptor describes a composing constraint. <code>ConstraintDescriptor</code>
-	 * instances of composing constraints reflect overridden parameter values in
-	 * {@link #getParameters()}  and {@link #getAnnotation()}.
-	 *
-	 * @return a set of <code>ConstraintDescriptor<code> objects or an empty set
-	 *         in case there are no composing constraints.
-	 */
-	Set<ConstraintDescriptor> getComposingConstraints();
+		/**
+		 * Return a set of composing <code>ConstraintDescriptor</code>s where each
+		 * descriptor describes a composing constraint. <code>ConstraintDescriptor</code>
+		 * instances of composing constraints reflect overridden parameter values in
+		 * {@link #getParameters()}  and {@link #getAnnotation()}.
+		 *
+		 * @return a set of <code>ConstraintDescriptor<code> objects or an empty set
+		 *         in case there are no composing constraints.
+		 */
+		Set<ConstraintDescriptor> getComposingConstraints();
 
-	/**
-	 * @return true if the constraint is annotated with @ReportAsViolationFromCompositeConstraint
-	 */
-	boolean isReportAsViolationFromCompositeConstraint();
-}
+		/**
+		 * @return true if the constraint is annotated with @ReportAsViolationFromCompositeConstraint
+		 */
+		boolean isReportAsViolationFromCompositeConstraint();
+	}
