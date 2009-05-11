b2d0145d88edfe97341502a28c776f2b5a1fda0c
Hardy Ferentschik <hibernate@ferentschik.de>
2009-05-11 10:54:59 +0000

    Made compilation work with Java5 - not sure why added cast is needed though.
    git-svn-id: https://svn.jboss.org/repos/hibernate/validator/trunk@16527 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-validator/src/test/java/org/hibernate/validation/engine/resolver/TraversableResolverTest.java
+++ hibernate-validator/src/test/java/org/hibernate/validation/engine/resolver/TraversableResolverTest.java
@@ -1,12 +1,13 @@
 package org.hibernate.validation.engine.resolver;
 
+import javax.validation.Configuration;
 import javax.validation.Validation;
-import javax.validation.ValidatorFactory;
 import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import javax.validation.groups.Default;
 
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 /**
  * @author Emmanuel Bernard
@@ -15,16 +16,18 @@ public class TraversableResolverTest {
 	@Test
 	public void testCorrectPathsAreRequested() {
 		Suit suit = new Suit();
-				suit.setTrousers( new Trousers() );
-				suit.setJacket( new Jacket() );
-				suit.setSize( 3333 );
-				suit.getTrousers().setLength( 32321 );
-				suit.getJacket().setWidth( 432432 );
+		suit.setTrousers( new Trousers() );
+		suit.setJacket( new Jacket() );
+		suit.setSize( 3333 );
+		suit.getTrousers().setLength( 32321 );
+		suit.getJacket().setWidth( 432432 );
 
-		SnifferTraversableResolver resolver = new SnifferTraversableResolver(suit);
-		ValidatorFactory factory = Validation.byDefaultProvider()
-				.configure().traversableResolver( resolver )
-				.buildValidatorFactory();
+		SnifferTraversableResolver resolver = new SnifferTraversableResolver( suit );
+
+		// TODO - Investigate why this cast is needed with Java 5. In Java 6 there is no problem.
+		Configuration<?> config = (Configuration<?>) Validation.byDefaultProvider().configure().traversableResolver( resolver );
+
+		ValidatorFactory factory = config.buildValidatorFactory();
 		Validator v = factory.getValidator();
 
 		//Raises an IllegalStateException if something goes wrong
