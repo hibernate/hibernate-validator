0c6dd207bc220db316a01931a332f6b6f8872502
Hardy Ferentschik <hibernate@ferentschik.de>
2009-10-15 12:37:13 +0000

    BVTCK-2
    git-svn-id: https://svn.jboss.org/repos/hibernate/validator/trunk@17764 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-validator/src/test/java/org/hibernate/validator/constraints/ConstraintValidatorContextTest.java
+++ hibernate-validator/src/test/java/org/hibernate/validator/constraints/ConstraintValidatorContextTest.java
@@ -17,19 +17,9 @@
 */
 package org.hibernate.validator.constraints;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.ConstraintViolation;
-import javax.validation.Payload;
 import javax.validation.Validator;
 
 import static org.testng.Assert.assertEquals;
@@ -40,7 +30,6 @@ import org.hibernate.validator.engine.ConstraintValidatorContextImpl;
 import org.hibernate.validator.engine.MessageAndPath;
 import org.hibernate.validator.engine.PathImpl;
 import org.hibernate.validator.util.TestUtil;
-import static org.hibernate.validator.util.TestUtil.assertCorrectConstraintTypes;
 import static org.hibernate.validator.util.TestUtil.assertCorrectPropertyPaths;
 import static org.hibernate.validator.util.TestUtil.assertNumberOfViolations;
 
@@ -134,7 +123,7 @@ public class ConstraintValidatorContextTest {
 				.addConstraintViolation();
 
 		messageAndPathList = context.getMessageAndPathList();
-		assertMessageAndPath( messageAndPathList.get( 0 ), message, "foo[test].bar[].fubar" );		
+		assertMessageAndPath( messageAndPathList.get( 0 ), message, "foo[test].bar[].fubar" );
 	}
 
 	@Test
@@ -155,20 +144,6 @@ public class ConstraintValidatorContextTest {
 		assertMessageAndPath( messageAndPathList.get( 1 ), message2, "" );
 	}
 
-	/**
-	 * HV-253
-	 */
-	@Test
-	public void propertyPathInIterable() {
-		Validator validator = TestUtil.getValidator();
-		Group group = new Group( Gender.MALE, new Person( Gender.FEMALE ) );
-
-		Set<ConstraintViolation<Group>> constraintViolations = validator.validate( group );
-		assertNumberOfViolations( constraintViolations, 1 );
-		assertCorrectPropertyPaths( constraintViolations, "persons[0]" );
-		assertCorrectConstraintTypes( constraintViolations, CompatiblePersons.class );
-	}
-
 	private ConstraintValidatorContextImpl createEmptyConstraintValidatorContextImpl() {
 		ConstraintValidatorContextImpl context = new ConstraintValidatorContextImpl(
 				PathImpl.createNewPath( null ), null
@@ -180,66 +155,5 @@ public class ConstraintValidatorContextTest {
 	private void assertMessageAndPath(MessageAndPath messageAndPath, String expectedMessage, String expectedPath) {
 		assertEquals( messageAndPath.getPath(), PathImpl.createPathFromString( expectedPath ), "Wrong path" );
 		assertEquals( messageAndPath.getMessage(), expectedMessage, "Wrong message" );
-	}
-
-	private enum Gender {
-		MALE, FEMALE
-	}
-
-	@CompatiblePersons
-	private class Group {
-		Gender gender;
-		List<Person> persons = new ArrayList<Person>();
-
-		public Group(Gender gender, Person... persons) {
-			this.gender = gender;
-			this.persons.addAll( Arrays.asList( persons ) );
-		}
-	}
-
-	private class Person {
-		Gender gender;
-
-		public Person(Gender gender) {
-			this.gender = gender;
-		}
-	}
-
-	@Target({ java.lang.annotation.ElementType.TYPE, java.lang.annotation.ElementType.ANNOTATION_TYPE })
-	@Retention(RetentionPolicy.RUNTIME)
-	@Constraint(validatedBy = { CompatiblePersonsValidator.class })
-	@Documented
-	public @interface CompatiblePersons {
-		String message() default "";
-
-		Class<?>[] groups() default { };
-
-		Class<? extends Payload>[] payload() default { };
-	}
-
-	public static class CompatiblePersonsValidator implements ConstraintValidator<CompatiblePersons, Group> {
-		public void initialize(CompatiblePersons constraintAnnotation) {
-		}
-
-		public boolean isValid(Group group, ConstraintValidatorContext constraintValidatorContext) {
-			if ( group == null ) {
-				return true;
-			}
-
-			constraintValidatorContext.disableDefaultConstraintViolation();
-
-			for ( int index = 0; index < group.persons.size(); index++ ) {
-				Person person = group.persons.get( index );
-				if ( !group.gender.equals( person.gender ) ) {
-					constraintValidatorContext
-							.buildConstraintViolationWithTemplate( "constraints.CompatiblePersons.gender.message" )
-							.addNode( "persons" )
-							.addNode( null ).inIterable().atIndex( index )
-							.addConstraintViolation();
-					return false;
-				}
-			}
-			return true;
-		}
 	}
 }
