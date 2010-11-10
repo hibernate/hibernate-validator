fd1408fb2e938eed9e7fc2ba0b1b461fd439faeb
Hardy Ferentschik <hibernate@ferentschik.de>
2010-11-10 11:32:20 +0100

    HV-390 Formatting

--- hibernate-validator/src/main/java/org/hibernate/validator/metadata/site/BeanConstraintSite.java
+++ hibernate-validator/src/main/java/org/hibernate/validator/metadata/site/BeanConstraintSite.java
@@ -20,7 +20,6 @@ package org.hibernate.validator.metadata.site;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-
 import javax.validation.ValidationException;
 
 import org.hibernate.validator.util.ReflectionHelper;
@@ -32,32 +31,32 @@ import org.hibernate.validator.util.ReflectionHelper;
  * @author Hardy Ferentschik
  * @author Gunnar Morling
  */
-public class BeanConstraintSite <T> implements ConstraintSite {
+public class BeanConstraintSite<T> implements ConstraintSite {
 
 	/**
 	 * The member the constraint was defined on.
 	 */
 	private final Member member;
-	
+
 	/**
 	 * The JavaBeans name of the field/property the constraint was placed on. {@code null} if this is a
 	 * class level constraint.
 	 */
 	private final String propertyName;
-	
+
 	/**
 	 * The class of the bean hosting this constraint.
 	 */
 	private final Class<T> beanClass;
-	
+
 	/**
 	 * @param beanClass The class in which the constraint is defined on
 	 * @param member The member on which the constraint is defined on, {@code null} if it is a class constraint}
 	 */
 	public BeanConstraintSite(Class<T> beanClass, Member member) {
-		
+
 		this.member = member;
-		
+
 		if ( this.member != null ) {
 			this.propertyName = ReflectionHelper.getPropertyName( member );
 			if ( member instanceof Method && propertyName == null ) { // can happen if member is a Method which does not follow the bean convention
@@ -65,7 +64,8 @@ public class BeanConstraintSite <T> implements ConstraintSite {
 						"Annotated methods must follow the JavaBeans naming convention. " + member.getName() + "() does not."
 				);
 			}
-		} else {
+		}
+		else {
 			this.propertyName = null;
 		}
 		this.beanClass = beanClass;
@@ -94,8 +94,8 @@ public class BeanConstraintSite <T> implements ConstraintSite {
 	 *         is returned of method or field access is used to access the value.
 	 */
 	public Object getValue(Object o) {
-		
-		if(member == null) {
+
+		if ( member == null ) {
 			return o;
 		}
 		else {
@@ -104,18 +104,18 @@ public class BeanConstraintSite <T> implements ConstraintSite {
 	}
 
 	public Type typeOfAnnotatedElement() {
-		Type t = null;
-		
-		if(member == null) {
-				t = beanClass;
+		Type t;
+
+		if ( member == null ) {
+			t = beanClass;
 		}
 		else {
-		t = ReflectionHelper.typeOf( member );
-			if ( t instanceof Class && ( ( Class<?> ) t ).isPrimitive() ) {
+			t = ReflectionHelper.typeOf( member );
+			if ( t instanceof Class && ( (Class<?>) t ).isPrimitive() ) {
 				t = ReflectionHelper.boxedType( t );
 			}
 		}
-		
+
 		return t;
 	}
 
