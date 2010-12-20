5dde575d89660fc6100ddca9f47e70f0a61613a6
Hardy Ferentschik <hibernate@ferentschik.de>
2010-12-20 12:40:26 +0100

    HV-402 Removing AbstractFormattingMessageInterpolator since there is now only one implementation

--- hibernate-validator/src/main/java/org/hibernate/validator/messageinterpolation/ValueFormatterMessageInterpolator.java
+++ hibernate-validator/src/main/java/org/hibernate/validator/messageinterpolation/ValueFormatterMessageInterpolator.java
@@ -19,6 +19,8 @@ package org.hibernate.validator.messageinterpolation;
 
 import java.util.IllegalFormatException;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.validation.MessageInterpolator;
 import javax.validation.ValidationException;
 
@@ -33,29 +35,120 @@ import javax.validation.ValidationException;
  *
  * @author Hardy Ferentschik
  */
-public class ValueFormatterMessageInterpolator extends AbstractFormattingMessageInterpolator
-		implements MessageInterpolator {
-	/**
-	 * Construct a message interpolator which delegates
-	 * the initial interpolation to the given MessageInterpolator and
-	 * uses {@link String#format} for interpolating the validated value.
-	 */
+public class ValueFormatterMessageInterpolator implements MessageInterpolator {
+	public static final String VALIDATED_VALUE_KEYWORD = "validatedValue";
+	public static final String VALIDATED_VALUE_FORMAT_SEPARATOR = ":";
+
+	private static final Pattern VALIDATED_VALUE_START_PATTERN = Pattern.compile( "\\$\\{" + VALIDATED_VALUE_KEYWORD );
+	private final MessageInterpolator delegate;
+	private final Locale defaultLocale;
+
 	public ValueFormatterMessageInterpolator() {
 		this( null );
 	}
 
-	/**
-	 * Construct a message interpolator which delegates
-	 * the initial interpolation to the given MessageInterpolator and
-	 * uses {@link String#format} for interpolating the validated value.
-	 *
-	 * @param userMessageInterpolator the user specified message interpolator
-	 */
 	public ValueFormatterMessageInterpolator(MessageInterpolator userMessageInterpolator) {
-		super( userMessageInterpolator );
+		defaultLocale = Locale.getDefault();
+		if ( userMessageInterpolator == null ) {
+			this.delegate = new ResourceBundleMessageInterpolator();
+		}
+		else {
+			this.delegate = userMessageInterpolator;
+		}
 	}
 
-	protected String interpolateValidatedValue(String expression, Object validatedValue, Locale locale) {
+	public String interpolate(String message, Context context) {
+		return interpolate( message, context, defaultLocale );
+	}
+
+	public String interpolate(String message, Context context, Locale locale) {
+		String tmp = delegate.interpolate( message, context, locale );
+		return interpolateMessage( tmp, context.getValidatedValue(), locale );
+	}
+
+	/**
+	 * Interpolate the validated value in the given message.
+	 *
+	 * @param message the message where validated value have to be interpolated
+	 * @param validatedValue the value of the object being validated
+	 * @param locale the {@code Locale} to use for message interpolation
+	 *
+	 * @return the interpolated message
+	 */
+	private String interpolateMessage(String message, Object validatedValue, Locale locale) {
+		String interpolatedMessage = message;
+		Matcher matcher = VALIDATED_VALUE_START_PATTERN.matcher( message );
+
+		while ( matcher.find() ) {
+			int nbOpenCurlyBrace = 1;
+			boolean isDoubleQuoteBloc = false;
+			boolean isSimpleQuoteBloc = false;
+			int lastIndex = matcher.end();
+
+			do {
+				char current = message.charAt( lastIndex );
+
+				if ( current == '\'' ) {
+					if ( !isDoubleQuoteBloc && !isEscaped( message, lastIndex ) ) {
+						isSimpleQuoteBloc = !isSimpleQuoteBloc;
+					}
+				}
+				else if ( current == '"' ) {
+					if ( !isSimpleQuoteBloc && !isEscaped( message, lastIndex ) ) {
+						isDoubleQuoteBloc = !isDoubleQuoteBloc;
+					}
+				}
+				else if ( !isDoubleQuoteBloc && !isSimpleQuoteBloc ) {
+					if ( current == '{' ) {
+						nbOpenCurlyBrace++;
+					}
+					else if ( current == '}' ) {
+						nbOpenCurlyBrace--;
+					}
+				}
+
+				lastIndex++;
+
+			} while ( nbOpenCurlyBrace > 0 && lastIndex < message.length() );
+
+			// The validated value expression seems correct
+			if ( nbOpenCurlyBrace == 0 ) {
+				String expression = message.substring( matcher.start(), lastIndex );
+				String expressionValue = interpolateValidatedValue( expression, validatedValue, locale );
+				interpolatedMessage = interpolatedMessage.replaceFirst(
+						Pattern.quote( expression ), Matcher.quoteReplacement( expressionValue )
+				);
+			}
+		}
+		return interpolatedMessage;
+	}
+
+	/**
+	 * Returns if the character at the given index in the String
+	 * is an escaped character (preceded by a backslash character).
+	 *
+	 * @param enclosingString the string which contain the given character
+	 * @param charIndex the index of the character
+	 *
+	 * @return true if the given character is escaped false otherwise
+	 */
+	private boolean isEscaped(String enclosingString, int charIndex) {
+		if ( charIndex < 0 || charIndex > enclosingString.length() ) {
+			throw new IndexOutOfBoundsException( "The given index must be between 0 and enclosingString.length() - 1" );
+		}
+		return charIndex > 0 && enclosingString.charAt( charIndex - 1 ) == '\\';
+	}
+
+	/**
+	 * Returns the value of the interpolated validated value.
+	 *
+	 * @param expression the expression to interpolate
+	 * @param validatedValue the value of the object being validated
+	 * @param locale the {@code Locale} to be used
+	 *
+	 * @return the interpolated value
+	 */
+	private String interpolateValidatedValue(String expression, Object validatedValue, Locale locale) {
 		String interpolatedValue;
 		int separatorIndex = expression.indexOf( VALIDATED_VALUE_FORMAT_SEPARATOR );
 
