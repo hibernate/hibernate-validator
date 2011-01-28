643c98aa64b859c07c566d6fdbfad6d63cfacfaf
kevin pollet <pollet.kevin@gmail.com>
2011-01-28 17:03:07 +0100

    HV-381: Add Emmanuel perf test (make sure to set log to ERROR before running)

--- hibernate-validator/src/test/java/org/hibernate/validator/test/engine/failfast/FailFastTest.java
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/engine/failfast/FailFastTest.java
@@ -1,10 +1,15 @@
 package org.hibernate.validator.test.engine.failfast;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.testng.annotations.Test;
 
@@ -90,12 +95,73 @@ public class FailFastTest {
 		assertNumberOfViolations( constraintViolations, 2 );
 	}
 
+	@Test
+	public void testFailSafePerf() {
+		final Validator regularValidator = TestUtil.getConfiguration().buildValidatorFactory().getValidator();
+		final Validator failFastValidator = TestUtil.getConfiguration().buildValidatorFactory().unwrap( HibernateValidatorFactory.class ).usingHibernateContext().failFast( true ).getValidator();
+
+		final int looptime = 50000;
+		for (int i = 0 ; i < looptime; i++) {
+			validateBatch(regularValidator);
+		}
+
+		for (int i = 0 ; i < looptime; i++) {
+			validateBatch(failFastValidator);
+		}
+
+		long start = System.nanoTime();
+		for (int i = 0 ; i < looptime; i++) {
+			validateBatch(regularValidator);
+		}
+		long timeOfRegular = System.nanoTime() - start;
+
+		start = System.nanoTime();
+		for (int i = 0 ; i < looptime; i++) {
+			validateBatch(failFastValidator);
+		}
+		long timeOfFailFast = System.nanoTime() - start;
+
+		System.err.println("Regular = " + timeOfRegular + "\n FailFast:" + timeOfFailFast);
+	}
+
+	private void validateBatch(Validator validator) {
+		validator.validate( buildA() );
+	}
+
+	static int i = 0;
+	A buildA() {
+		A a = new A();
+		a.b = "bbb" + i++;
+		a.file = "test" + i++ + ".txt";
+		a.bs.add( buildB() );
+		a.bs.add( buildB() );
+		a.bs.add( buildB() );
+		a.bs.add( buildB() );
+		return a;
+	}
+
+	B buildB() {
+		B b = new B();
+		b.size = 45 + i++;
+		return b;
+	}
+
 	class A {
 		@NotNull
 		String b;
 
-		@NotNull
-		@Email
+		@NotNull @Email
 		String c;
+
+		@Pattern(regexp = ".*\\.txt$")
+		String file;
+
+		@Valid
+		Set<B> bs = new HashSet<B>();
+	}
+
+	class B {
+		@Min(value = 10) @Max(value=30) @NotNull
+		Integer size;
 	}
 }
