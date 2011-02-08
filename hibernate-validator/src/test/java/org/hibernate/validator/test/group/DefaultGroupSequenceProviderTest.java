b6c0c3b76bb283bf81c97ac1eb73d92e0b9f447a
Kevin Pollet <pollet.kevin@gmail.com>
2011-02-08 14:56:10 +0100

    HV-94: Move provider tests to group subpackage.

--- hibernate-validator/src/test/java/org/hibernate/validator/test/group/DefaultGroupSequenceProviderTest.java
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/group/DefaultGroupSequenceProviderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hibernate.validator.test.engine.groups.provider;
+package org.hibernate.validator.test.group;
 
 import java.util.ArrayList;
 import java.util.List;
