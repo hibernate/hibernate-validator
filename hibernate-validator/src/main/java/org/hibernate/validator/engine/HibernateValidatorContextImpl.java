bf4b0eb873d825cce9f18ba9d1b6b5ea45bd3ece
Hardy Ferentschik <hibernate@ferentschik.de>
2011-02-09 15:34:50 +0100

    HV-381 Formatting

--- hibernate-validator/src/main/java/org/hibernate/validator/engine/HibernateValidatorContextImpl.java
+++ hibernate-validator/src/main/java/org/hibernate/validator/engine/HibernateValidatorContextImpl.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.hibernate.validator.engine;
 
 import javax.validation.ConstraintValidatorFactory;
