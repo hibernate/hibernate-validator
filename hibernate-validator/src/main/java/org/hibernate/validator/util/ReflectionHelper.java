3287d06ae44196d866baa64981db8a9d4ded3ccf
Hardy Ferentschik <hibernate@ferentschik.de>
2011-02-14 12:50:21 +0100

    HV-430 Updating license header and javadoc

--- hibernate-validator/src/main/java/org/hibernate/validator/util/ReflectionHelper.java
+++ hibernate-validator/src/main/java/org/hibernate/validator/util/ReflectionHelper.java
@@ -1,19 +1,19 @@
 /*
-* JBoss, Home of Professional Open Source
-* Copyright 2010, Red Hat, Inc. and/or its affiliates, and individual contributors
-* by the @authors tag. See the copyright.txt in the distribution for a
-* full listing of individual contributors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,  
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hibernate.validator.util;
 
 import java.beans.Introspector;
@@ -494,14 +494,13 @@ public final class ReflectionHelper {
 		return map.get( key );
 	}
 
-
 	/**
-	 * Returns the field with the specified name or <code>null</code> if it does not exist.
+	 * Returns the field with the specified name or {@code null} if it does not exist.
 	 *
-	 * @param clazz The class to check.
-	 * @param fieldName The field name.
+	 * @param clazz The class to check
+	 * @param fieldName The field name
 	 *
-	 * @return Returns the field with the specified name or <code>null</code> if it does not exist.
+	 * @return Returns the field with the specified name or {@code null} if it does not exist.
 	 */
 	public static Field getField(Class<?> clazz, String fieldName) {
 		GetField action = GetField.action( clazz, fieldName );
@@ -516,12 +515,12 @@ public final class ReflectionHelper {
 	}
 
 	/**
-	 * Returns the declared field with the specified name or <code>null</code> if it does not exist.
+	 * Returns the declared field with the specified name or {@code null} if it does not exist.
 	 *
-	 * @param clazz The class to check.
-	 * @param fieldName The field name.
+	 * @param clazz The class to check
+	 * @param fieldName The field name
 	 *
-	 * @return Returns the declared field with the specified name or <code>null</code> if it does not exist.
+	 * @return Returns the declared field with the specified name or {@code null} if it does not exist.
 	 */
 	public static Field getDeclaredField(Class<?> clazz, String fieldName) {
 		GetDeclaredField action = GetDeclaredField.action( clazz, fieldName );
