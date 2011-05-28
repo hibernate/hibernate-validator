a1705234de2c8ccf27261b786a6fadee392e1eba
Kevin Pollet <pollet.kevin@gmail.com>
2011-05-28 11:57:22 +0200

    HV-461 Cleanup ValidatorUtil class and adding comments.

--- hibernate-validator/src/test/java/org/hibernate/validator/test/util/ValidatorUtil.java
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/util/ValidatorUtil.java
@@ -50,80 +50,165 @@ public class ValidatorUtil {
 	private ValidatorUtil() {
 	}
 
+	/**
+	 * Returns a configured instance of {@code Validator}. This validator is configured to use a
+	 * {@link DummyTraversableResolver}. This method also sets the default locale to english.
+	 *
+	 * @return an instance of {@code Validator}.
+	 */
 	public static Validator getValidator() {
 		if ( hibernateValidator == null ) {
-			Configuration<HibernateValidatorConfiguration> configuration = getConfiguration( Locale.ENGLISH );
+			final Configuration<HibernateValidatorConfiguration> configuration = getConfiguration();
 			configuration.traversableResolver( new DummyTraversableResolver() );
 			hibernateValidator = configuration.buildValidatorFactory().getValidator();
 		}
 		return hibernateValidator;
 	}
 
+	/**
+	 * Returns a configured instance of {@code Validator} initialized with the given constraint mappings. This validator
+	 * is configured to use a {@link DummyTraversableResolver}. This method also sets the default locale to english.
+	 *
+	 * @param mappings The constraint mappings.
+	 *
+	 * @return an instance of {@code Validator}.
+	 */
 	public static Validator getValidatorForProgrammaticMapping(ConstraintMapping... mappings) {
 		assertNotNull( mappings );
-		HibernateValidatorConfiguration config = getConfiguration( HibernateValidator.class );
+		final HibernateValidatorConfiguration config = getConfiguration( HibernateValidator.class );
+		config.traversableResolver( new DummyTraversableResolver() );
 		for ( ConstraintMapping mapping : mappings ) {
 			config.addMapping( mapping );
 		}
 		return config.buildValidatorFactory().getValidator();
 	}
 
+	/**
+	 * Returns an instance of {@code MethodValidator} which can be used to validate method constraints. This validator
+	 * is constructed by getting an instance of {@code Validator} with the method {@link ValidatorUtil#getValidator()}.
+	 *
+	 * @return an instance of {@code MethodValidator}.
+	 */
 	public static MethodValidator getMethodValidator() {
 		return getValidator().unwrap( MethodValidator.class );
 	}
 
+	/**
+	 * Returns the {@code Configuration} object for Hibernate Validator. This method also sets the default locale to
+	 * english.
+	 *
+	 * @return an instance of {@code Configuration} for Hibernate Validator.
+	 */
 	public static Configuration<HibernateValidatorConfiguration> getConfiguration() {
 		return getConfiguration( HibernateValidator.class, Locale.ENGLISH );
 	}
 
+	/**
+	 * Returns the {@code Configuration} object for Hibernate Validator. This method also sets the default locale to
+	 * the given locale.
+	 *
+	 * @param locale The default locale to set.
+	 *
+	 * @return an instance of {@code Configuration} for Hibernate Validator.
+	 */
 	public static Configuration<HibernateValidatorConfiguration> getConfiguration(Locale locale) {
 		return getConfiguration( HibernateValidator.class, locale );
 	}
 
+	/**
+	 * Returns the {@code Configuration} object for the given validation provider type. This method also sets the
+	 * default locale to english.
+	 *
+	 * @param type The validation provider type.
+	 *
+	 * @return an instance of {@code Configuration}.
+	 */
 	public static <T extends Configuration<T>, U extends ValidationProvider<T>> T getConfiguration(Class<U> type) {
 		return getConfiguration( type, Locale.ENGLISH );
 	}
 
+	/**
+	 * Returns the {@code Configuration} object for the given validation provider type. This method also sets the
+	 * default locale to the given locale.
+	 *
+	 * @param type The validation provider type.
+	 * @param locale The default locale to set.
+	 *
+	 * @return an instance of {@code Configuration}.
+	 */
 	public static <T extends Configuration<T>, U extends ValidationProvider<T>> T getConfiguration(Class<U> type, Locale locale) {
 		Locale.setDefault( locale );
 		return Validation.byProvider( type ).configure();
 	}
 
+	/**
+	 * Returns the {@code PropertyDescriptor} corresponding to the property contained in the given class.
+	 *
+	 * @param clazz The class containing the given property.
+	 * @param property The property name.
+	 *
+	 * @return an instance of {@code PropertyDescriptor} or {@code null} if does not exists or has no constraint.
+	 *
+	 * @see javax.validation.metadata.BeanDescriptor#getConstraintsForProperty(String)
+	 */
 	public static PropertyDescriptor getPropertyDescriptor(Class<?> clazz, String property) {
-		Validator validator = getValidator();
-		return validator.getConstraintsForClass( clazz ).getConstraintsForProperty( property );
+		return getValidator().getConstraintsForClass( clazz ).getConstraintsForProperty( property );
 	}
 
+	/**
+	 * Returns the {@code TypeDescriptor} corresponding to the given type.
+	 *
+	 * @param clazz The type.
+	 *
+	 * @return an instance of {@code TypeDescriptor} for the given type, never {@code null}
+	 */
 	public static TypeDescriptor getTypeDescriptor(Class<?> clazz) {
-
-		TypeDescriptor typeDescriptor = getMethodValidator().getConstraintsForType( clazz );
-		assertNotNull( typeDescriptor );
-
-		return typeDescriptor;
+		return getMethodValidator().getConstraintsForType( clazz );
 	}
 
+	/**
+	 * Returns the {@code MethodDescriptor} for the given method signature in the given class.
+	 *
+	 * @param clazz The class.
+	 * @param methodName The method name.
+	 * @param parameterTypes The method parameter types.
+	 *
+	 * @return an instance of {@code MethodDescriptor} for the given method signature or {@code null} if does not exists.
+	 *
+	 * @see TypeDescriptor#getConstraintsForMethod(String, Class[])
+	 */
 	public static MethodDescriptor getMethodDescriptor(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
-
-		MethodDescriptor methodDescriptor = getTypeDescriptor( clazz ).getConstraintsForMethod(
-				methodName, parameterTypes
-		);
-		assertNotNull( methodDescriptor );
-
-		return methodDescriptor;
+		return getTypeDescriptor( clazz ).getConstraintsForMethod( methodName, parameterTypes );
 	}
 
+	/**
+	 * Returns the {@code ParameterDescriptor} for the parameter at the given index within the method, corresponding to
+	 * the given signature, within the given class.
+	 *
+	 * @param clazz The class.
+	 * @param methodName The method name.
+	 * @param parameterTypes The method parameter types.
+	 * @param parameterIndex The parameter index.
+	 *
+	 * @return an instance of {@code ParameterDescriptor}.
+	 */
 	public static ParameterDescriptor getParameterDescriptor(Class<?> clazz, String methodName, Class<?>[] parameterTypes, int parameterIndex) {
-
-		MethodDescriptor methodDescriptor = getMethodDescriptor( clazz, methodName, parameterTypes );
-		ParameterDescriptor parameterDescriptor = methodDescriptor.getParameterConstraints().get( parameterIndex );
-		assertNotNull( parameterDescriptor );
-
-		return parameterDescriptor;
+		final MethodDescriptor methodDescriptor = getMethodDescriptor( clazz, methodName, parameterTypes );
+		assertNotNull( methodDescriptor );
+		return methodDescriptor.getParameterConstraints().get( parameterIndex );
 	}
 
+	/**
+	 * Returns an instance of proxy which dispatches method invocation to the given validation invocation handler.
+	 *
+	 * @param handler The validation invocation handler.
+	 *
+	 * @return a proxy instance configured with the given handler.
+	 *
+	 * @see ValidationInvocationHandler
+	 */
 	public static Object getMethodValidationProxy(ValidationInvocationHandler handler) {
 		final Class<?> wrappedObjectClass = handler.getWrapped().getClass();
-
 		return Proxy.newProxyInstance(
 				wrappedObjectClass.getClassLoader(),
 				wrappedObjectClass.getInterfaces(),
