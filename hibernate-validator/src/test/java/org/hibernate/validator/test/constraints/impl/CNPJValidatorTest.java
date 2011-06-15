6786ae2aa01dffb90f1eafe319a82d8e867e6592
George Gastaldi <gegastaldi@gmail.com>
2011-06-15 18:56:58 -0300

    HV-491: Added CNPJ validator

---
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/constraints/impl/CNPJValidatorTest.java
@@ -0,0 +1,47 @@
+package org.hibernate.validator.test.constraints.impl;
+
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertNumberOfViolations;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
+import org.hibernate.validator.constraints.CNPJ;
+import org.hibernate.validator.testutil.ValidatorUtil;
+import org.testng.annotations.Test;
+
+public class CNPJValidatorTest {
+
+	/**
+	 * HV-491
+	 */
+	@Test
+	public void testCorrectFormattedCNPJWithReportAsSingleViolation() {
+		Set<ConstraintViolation<Company>> violations = ValidatorUtil.getValidator().validate(
+				new Company( "91.509.901/0001-69" ) );
+		assertNumberOfViolations( violations, 0 );
+	}
+
+	/**
+	 * HV-491
+	 */
+	@Test
+	public void testIncorrectFormattedCNPJWithReportAsSingleViolation() {
+		Set<ConstraintViolation<Company>> violations = ValidatorUtil.getValidator().validate(
+				new Company( "ABCDEF" ) );
+		assertNumberOfViolations( violations, 1 );
+	}
+
+	public static class Company {
+		@CNPJ
+		private String cnpj;
+
+		public Company(String cnpj) {
+			this.cnpj = cnpj;
+		}
+
+		public String getCnpj() {
+			return cnpj;
+		}
+	}
+}
