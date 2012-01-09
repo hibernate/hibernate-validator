a9657584c4fa4b603c2b58aaeb677e25db698542
Hardy Ferentschik <hibernate@ferentschik.de>
2012-01-09 15:51:37 +0100

    HV-551 SizeValidatorForCollection should not use generics.

--- hibernate-validator/src/test/java/org/hibernate/validator/test/constraints/impl/SizeValidatorTest.java
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/constraints/impl/SizeValidatorTest.java
@@ -20,8 +20,12 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.validation.constraints.Size;
 
 import org.testng.annotations.Test;
@@ -35,10 +39,12 @@ import org.hibernate.validator.constraints.impl.SizeValidatorForArraysOfFloat;
 import org.hibernate.validator.constraints.impl.SizeValidatorForArraysOfInt;
 import org.hibernate.validator.constraints.impl.SizeValidatorForArraysOfLong;
 import org.hibernate.validator.constraints.impl.SizeValidatorForArraysOfShort;
+import org.hibernate.validator.constraints.impl.SizeValidatorForCharSequence;
 import org.hibernate.validator.constraints.impl.SizeValidatorForCollection;
 import org.hibernate.validator.constraints.impl.SizeValidatorForMap;
-import org.hibernate.validator.constraints.impl.SizeValidatorForCharSequence;
+import org.hibernate.validator.testutil.ConstraintViolationAssert;
 import org.hibernate.validator.testutil.TestForIssue;
+import org.hibernate.validator.testutil.ValidatorUtil;
 import org.hibernate.validator.util.annotationfactory.AnnotationDescriptor;
 import org.hibernate.validator.util.annotationfactory.AnnotationFactory;
 
@@ -47,6 +53,7 @@ import static org.testng.Assert.assertTrue;
 
 /**
  * @author Alaa Nassef
+ * @author Hardy Feretnschik
  */
 public class SizeValidatorTest {
 
@@ -162,6 +169,14 @@ public class SizeValidatorTest {
 		assertFalse( validator.isValid( new MyCustomStringImpl( "abc" ), null ) );
 	}
 
+	@Test
+	@TestForIssue(jiraKey = "HV-551")
+	public void testGenericsExtendsFoo() throws Exception {
+		Validator validator = ValidatorUtil.getValidator();
+		Set<ConstraintViolation<Foo>> violations = validator.validate( new Foo() );
+		ConstraintViolationAssert.assertCorrectConstraintViolationMessages( violations, "there can only be one" );
+	}
+
 	private <T> ConstraintValidator<Size, T> getValidatorMin1Max2(Class<?> validatorClass) throws Exception {
 		AnnotationDescriptor<Size> descriptor = new AnnotationDescriptor<Size>( Size.class );
 		descriptor.setValue( "min", 1 );
@@ -189,5 +204,10 @@ public class SizeValidatorTest {
 
 		array = (T) Array.newInstance( arrayType.getComponentType(), 3 );
 		assertFalse( validator.isValid( array, null ) );
+	}
+
+	private class Foo {
+		@Size(min = 1, max = 1, message = "there can only be one")
+		private List<? extends Foo> listOfSubClasses = new ArrayList<Foo>();
 	}
 }
