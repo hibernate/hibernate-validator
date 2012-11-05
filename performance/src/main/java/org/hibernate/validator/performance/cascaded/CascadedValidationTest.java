135e64f1074c912f5a09982cf028121ef44c665d
Hardy Ferentschik <hibernate@ferentschik.de>
2012-11-05 23:04:38 +0100

    HV-621 Adding a multi threaded test for profiling purposes

--- performance/src/main/java/org/hibernate/validator/performance/cascaded/CascadedValidationTest.java
+++ performance/src/main/java/org/hibernate/validator/performance/cascaded/CascadedValidationTest.java
@@ -18,6 +18,10 @@ package org.hibernate.validator.performance.cascaded;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
 import javax.validation.Validation;
@@ -28,13 +32,15 @@ import javax.validation.constraints.NotNull;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Hardy Ferentschik
  */
 public class CascadedValidationTest {
 	private static Validator validator;
+	private static final int NUMBER_OF_RUNNABLES = 10000;
+	private static final int SIZE_OF_THREAD_POOL = 50;
 
 	@BeforeClass
 	public static void setupValidatorInstance() {
@@ -57,6 +63,19 @@ public class CascadedValidationTest {
 		assertEquals( 0, violations.size() );
 	}
 
+	@Test
+	public void testCascadedValidationMultiThreaded() throws Exception {
+		CountDownLatch startLatch = new CountDownLatch( 1 );
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool( SIZE_OF_THREAD_POOL );
+		for ( int i = 0; i <= NUMBER_OF_RUNNABLES; i++ ) {
+			Runnable run = new TestRunner( startLatch );
+			executor.execute( run );
+		}
+		executor.shutdown();
+		startLatch.countDown(); //start!
+		executor.awaitTermination( 600, TimeUnit.SECONDS );
+	}
+
 	public class Person {
 		@NotNull
 		String name;
@@ -71,6 +90,29 @@ public class CascadedValidationTest {
 		public Person addFriend(Person friend) {
 			friends.add( friend );
 			return this;
+		}
+	}
+
+	public class TestRunner implements Runnable {
+		private static final int NUMBER_OF_VALIDATION_ITERATIONS = 1000;
+		private final CountDownLatch latch;
+
+		public TestRunner(CountDownLatch latch) {
+			this.latch = latch;
+		}
+
+		@Override
+		public void run() {
+			try {
+				latch.await();
+			}
+			catch ( InterruptedException e ) {
+				e.printStackTrace();
+				return;
+			}
+			for ( int i = 0; i < NUMBER_OF_VALIDATION_ITERATIONS; i++ ) {
+				testCascadedValidation();
+			}
 		}
 	}
 }
