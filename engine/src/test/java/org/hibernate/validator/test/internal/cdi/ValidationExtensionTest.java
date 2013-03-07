23acfd402d348dfae4d836fa21ec85a5f5763106
Hardy Ferentschik <hibernate@ferentschik.de>
2013-03-07 13:57:56 +0100

    HV-740 Removing beans.xml. Interceptor is now registered programmatically via BeforeBeanDiscovery.addAnnotatedType(). Also using @Priority to set the interceptor priority
    This way not the whole jar gets scanned for beans. Also started to use metadata to determine whether a method needs to be intercepted

--- engine/src/test/java/org/hibernate/validator/test/internal/cdi/ValidationExtensionTest.java
+++ engine/src/test/java/org/hibernate/validator/test/internal/cdi/ValidationExtensionTest.java
@@ -18,28 +18,17 @@ package org.hibernate.validator.test.internal.cdi;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
-import javax.enterprise.inject.spi.AnnotatedCallable;
-import javax.enterprise.inject.spi.AnnotatedConstructor;
-import javax.enterprise.inject.spi.AnnotatedMethod;
-import javax.enterprise.inject.spi.AnnotatedParameter;
-import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.BeforeBeanDiscovery;
-import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import javax.enterprise.inject.spi.ProcessBean;
 import javax.enterprise.util.AnnotationLiteral;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import javax.validation.constraints.NotNull;
 
-import org.easymock.Capture;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -47,32 +36,21 @@ import org.hibernate.validator.cdi.HibernateValidator;
 import org.hibernate.validator.internal.cdi.ValidationExtension;
 import org.hibernate.validator.internal.cdi.ValidatorBean;
 import org.hibernate.validator.internal.cdi.ValidatorFactoryBean;
-import org.hibernate.validator.internal.cdi.interceptor.MethodValidated;
-import org.hibernate.validator.internal.util.annotationfactory.AnnotationDescriptor;
-import org.hibernate.validator.internal.util.annotationfactory.AnnotationFactory;
 
-import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
-import static org.hibernate.validator.internal.util.CollectionHelper.newHashSet;
-import static org.testng.Assert.assertTrue;
 
 /**
  * @author Hardy Ferentschik
  */
 @Test(singleThreaded = true) // needs to run single threaded, because the mocks are shared across test methods
-public class ValidationExtensionTest<T> {
+public class ValidationExtensionTest {
 	private ValidationExtension extension;
 	private AfterBeanDiscovery afterBeanDiscoveryMock;
 	private ProcessBean processBeanMock;
-	private BeforeBeanDiscovery beforeBeanDiscoveryMock;
-	private ProcessAnnotatedType<T> processAnnotatedTypeMock;
-	private AnnotatedType<T> annotatedTypeMock;
-	private AnnotatedMethod<T> annotatedMethodMock;
-	private AnnotatedConstructor<T> annotatedConstructorMock;
 	private Bean<ValidatorFactory> validatorFactoryBeanMock;
 	private Bean<Validator> validatorBeanMock;
 	private BeanManager beanManagerMock;
@@ -83,11 +61,6 @@ public class ValidationExtensionTest<T> {
 		extension = new ValidationExtension();
 		afterBeanDiscoveryMock = createMock( AfterBeanDiscovery.class );
 		processBeanMock = createMock( ProcessBean.class );
-		beforeBeanDiscoveryMock = createMock( BeforeBeanDiscovery.class );
-		processAnnotatedTypeMock = createMock( ProcessAnnotatedType.class );
-		annotatedTypeMock = createMock( AnnotatedType.class );
-		annotatedMethodMock = createMock( AnnotatedMethod.class );
-		annotatedConstructorMock = createMock( AnnotatedConstructor.class );
 		beanManagerMock = createMock( BeanManager.class );
 		validatorFactoryBeanMock = createMock( Bean.class );
 		validatorBeanMock = createMock( Bean.class );
@@ -193,113 +166,6 @@ public class ValidationExtensionTest<T> {
 	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testProcessAnnotatedTypeNullParameter() {
 		extension.processAnnotatedType( null );
-	}
-
-	@Test
-	public void testConstrainedMethodGetsInterceptorBidingAdded() {
-		AnnotationDescriptor<NotNull> descriptor = new AnnotationDescriptor<NotNull>( NotNull.class );
-		Annotation notNull = AnnotationFactory.create( descriptor );
-		setupMocks( annotatedMethodMock, notNull );
-
-		Capture<AnnotatedType<T>> capturedType = new Capture<AnnotatedType<T>>();
-		processAnnotatedTypeMock.setAnnotatedType( capture( capturedType ) );
-
-		// get the mocks ready
-		replay( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock, annotatedConstructorMock );
-
-		// run the code
-		extension.processAnnotatedType( processAnnotatedTypeMock );
-
-		// verify the mocks
-		verify( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock, annotatedConstructorMock );
-
-		// check the captured type has @MethodValidated added
-		Set<AnnotatedMethod<? super T>> methods = capturedType.getValue().getMethods();
-		assertTrue( methods.size() == 1, "We still should only have a single method" );
-		AnnotatedMethod<?> method = methods.iterator().next();
-		assertTrue(
-				method.isAnnotationPresent( MethodValidated.class ),
-				"The @MethodValidated annotation method should have been added"
-		);
-	}
-
-	@Test
-	public void testConstrainedConstructorGetsInterceptorBidingAdded() {
-		AnnotationDescriptor<NotNull> descriptor = new AnnotationDescriptor<NotNull>( NotNull.class );
-		Annotation notNull = AnnotationFactory.create( descriptor );
-		setupMocks( annotatedConstructorMock, notNull );
-
-		Capture<AnnotatedType<T>> capturedType = new Capture<AnnotatedType<T>>();
-		processAnnotatedTypeMock.setAnnotatedType( capture( capturedType ) );
-
-		// get the mocks ready
-		replay( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock, annotatedConstructorMock );
-
-		// run the code
-		extension.processAnnotatedType( processAnnotatedTypeMock );
-
-		// verify the mocks
-		verify( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock, annotatedConstructorMock );
-
-		// check the captured type has @MethodValidated added
-		Set<AnnotatedConstructor<T>> constructors = capturedType.getValue().getConstructors();
-		assertTrue( constructors.size() == 1, "We still should only have a single constructor" );
-		AnnotatedConstructor<?> constructor = constructors.iterator().next();
-		assertTrue(
-				constructor.isAnnotationPresent( MethodValidated.class ),
-				"The @MethodValidated annotation method should have been added"
-		);
-	}
-
-	@Test
-	public void testUnConstrainedMethodDoesNotGetInterceptorBidingAdded() {
-		setupMocks( annotatedMethodMock );
-
-		// get the mocks ready
-		replay( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock );
-
-		// run the code
-		extension.processAnnotatedType( processAnnotatedTypeMock );
-
-		// verify the mocks
-		verify( processAnnotatedTypeMock, annotatedTypeMock, annotatedMethodMock );
-	}
-
-	private void setupMocks(AnnotatedCallable<T> callable, Annotation... constraintAnnotations) {
-		expect( processAnnotatedTypeMock.getAnnotatedType() ).andReturn( annotatedTypeMock );
-
-		Set<AnnotatedConstructor<T>> constructors = newHashSet();
-		Set<Annotation> constructorAnnotations = newHashSet();
-		if ( callable instanceof AnnotatedConstructor ) {
-			constructors.add( (AnnotatedConstructor<T>) callable );
-			Collections.addAll( constructorAnnotations, constraintAnnotations );
-		}
-		expect( annotatedTypeMock.getConstructors() ).andReturn( constructors );
-
-		Set<AnnotatedMethod<? super T>> methods = newHashSet();
-		Set<Annotation> methodAnnotations = newHashSet();
-		if ( callable instanceof AnnotatedMethod ) {
-			methods.add( (AnnotatedMethod<T>) callable );
-			Collections.addAll( methodAnnotations, constraintAnnotations );
-		}
-		expect( annotatedTypeMock.getMethods() ).andReturn( methods );
-
-		if ( callable instanceof AnnotatedConstructor ) {
-			expect( annotatedConstructorMock.getAnnotations() ).andReturn( constructorAnnotations );
-		}
-		else {
-			expect( annotatedMethodMock.getAnnotations() ).andReturn( methodAnnotations );
-		}
-
-		if ( constraintAnnotations.length == 0 ) {
-			// if there is no constraint annotation on the method the parameters get checked
-			expect( annotatedMethodMock.getParameters() ).andReturn( new ArrayList<AnnotatedParameter<T>>() );
-		}
-		else {
-			// if we have found a constraint annotation we expect another call to getConstructors and getMethods when the wrapped type gets build
-			expect( annotatedTypeMock.getConstructors() ).andReturn( constructors );
-			expect( annotatedTypeMock.getMethods() ).andReturn( methods );
-		}
 	}
 }
 
