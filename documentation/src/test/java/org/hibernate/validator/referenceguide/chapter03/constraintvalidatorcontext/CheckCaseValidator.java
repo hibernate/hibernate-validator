fe408b72f2ae743621aeca1fe11f47768920bc1d
Gunnar Morling <gunnar.morling@googlemail.com>
2013-04-03 10:43:51 +0200

    HV-725 Chapter 3 updates - adding examples to source tree and updating docs.

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter03/constraintvalidatorcontext/CheckCaseValidator.java
@@ -0,0 +1,39 @@
+package org.hibernate.validator.referenceguide.chapter03.constraintvalidatorcontext;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CheckCaseValidator implements ConstraintValidator<CheckCase, String> {
+
+	private CaseMode caseMode;
+
+	@Override
+	public void initialize(CheckCase constraintAnnotation) {
+		this.caseMode = constraintAnnotation.value();
+	}
+
+	@Override
+	public boolean isValid(String object, ConstraintValidatorContext constraintContext) {
+		if ( object == null ) {
+			return true;
+		}
+
+		boolean isValid;
+		if ( caseMode == CaseMode.UPPER ) {
+			isValid = object.equals( object.toUpperCase() );
+		}
+		else {
+			isValid = object.equals( object.toLowerCase() );
+		}
+
+		if ( !isValid ) {
+			constraintContext.disableDefaultConstraintViolation();
+			constraintContext.buildConstraintViolationWithTemplate(
+					"{org.hibernate.validator.referenceguide.chapter03.constraintvalidatorcontext.CheckCase.message}"
+			)
+					.addConstraintViolation();
+		}
+
+		return isValid;
+	}
+}
