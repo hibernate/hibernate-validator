fe292321f1835769790d2042baec868f59ed889d
Gunnar Morling <gunnar.morling@googlemail.com>
2013-04-26 09:22:10 +0200

    HV-725 Misc. formatting and clean-ups

--- documentation/src/test/java/org/hibernate/validator/referenceguide/chapter05/groupsequenceprovider/RentalCarGroupSequenceProvider.java
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter05/groupsequenceprovider/RentalCarGroupSequenceProvider.java
@@ -7,6 +7,7 @@ import org.hibernate.validator.referenceguide.chapter05.CarChecks;
 import org.hibernate.validator.spi.group.DefaultGroupSequenceProvider;
 
 public class RentalCarGroupSequenceProvider implements DefaultGroupSequenceProvider<RentalCar> {
+	@Override
 	public List<Class<?>> getValidationGroups(RentalCar car) {
 		List<Class<?>> defaultGroupSequence = new ArrayList<Class<?>>();
 		defaultGroupSequence.add( RentalCar.class );
