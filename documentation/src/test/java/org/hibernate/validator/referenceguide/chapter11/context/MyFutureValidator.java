46b97021c113b4eee66b75071c5d56dbd298f5e9
Hardy Ferentschik <hibernate@ferentschik.de>
2013-07-10 17:27:30 +0200

    HV-701 Updating online docs

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter11/context/MyFutureValidator.java
@@ -0,0 +1,36 @@
+package org.hibernate.validator.referenceguide.chapter11.context;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Future;
+
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+
+public class MyFutureValidator implements ConstraintValidator<Future, Date> {
+
+	public void initialize(Future constraintAnnotation) {
+	}
+
+	public boolean isValid(Date value, ConstraintValidatorContext context) {
+		Date now = GregorianCalendar.getInstance().getTime();
+
+		if ( value.before( now ) ) {
+			HibernateConstraintValidatorContext hibernateContext =
+					context.unwrap( HibernateConstraintValidatorContext.class );
+
+			hibernateContext.disableDefaultConstraintViolation();
+			hibernateContext.addMessageParameter( "now", now )
+					.buildConstraintViolationWithTemplate( "Must be after ${now}" )
+					.addConstraintViolation();
+
+			return false;
+		}
+
+		return true;
+	}
+}
+
+
+
