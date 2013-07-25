7edd9172f2940c67838dcec16ddfc3bb040bb578
Gunnar Morling <gunnar.morling@googlemail.com>
2013-07-25 09:32:58 +0200

    HV-642 Updating reference documentation

--- documentation/src/test/java/org/hibernate/validator/referenceguide/chapter11/constraintapi/RentalCar.java
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter11/constraintapi/RentalCar.java
@@ -4,6 +4,10 @@ public class RentalCar extends Car {
 
 	private String rentalStation;
 
+	public RentalCar(String manufacturer) {
+		super( manufacturer );
+	}
+
 	public String getRentalStation() {
 		return rentalStation;
 	}
