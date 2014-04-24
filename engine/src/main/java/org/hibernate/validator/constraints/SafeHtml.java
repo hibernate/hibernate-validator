0972d34f8d7ca308f24a3665da1cd4191f68f95c
Gunnar Morling <gunnar.morling@googlemail.com>
2014-04-24 13:40:33 +0200

    HV-873 Clarifying scope of @SafeHtml constraint in JavaDocs

--- engine/src/main/java/org/hibernate/validator/constraints/SafeHtml.java
+++ engine/src/main/java/org/hibernate/validator/constraints/SafeHtml.java
@@ -33,6 +33,10 @@ import static org.hibernate.validator.constraints.SafeHtml.WhiteListType.RELAXED
 /**
  * Validate a rich text value provided by the user to ensure that it contains no malicious code, such as embedded
  * &lt;script&gt; elements.
+ * <p>
+ * Note that this constraint assumes you want to validate input which represents a body fragment of an HTML document. If
+ * you instead want to validate input which represents a complete HTML document, add the {@code html}, {@code head} and
+ * {@code body} tags to the used whitelist as required.
  *
  * @author George Gastaldi
  */
