28829eb94ae27ec9121c9d59c718637556f61952
Hardy Ferentschik <hibernate@ferentschik.de>
2014-10-09 16:26:32 +0200

    HV-828 Doc updates for constraint definition contributions

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter11/constraintdefinition/ValidPassengerCount.java
@@ -0,0 +1,27 @@
+package org.hibernate.validator.referenceguide.chapter11.constraintdefinition;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = { })
+@Documented
+public @interface ValidPassengerCount {
+
+	String message() default "{org.hibernate.validator.referenceguide.chapter11.constraintdefinition.ValidPassengerCount.message}";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+}
+
