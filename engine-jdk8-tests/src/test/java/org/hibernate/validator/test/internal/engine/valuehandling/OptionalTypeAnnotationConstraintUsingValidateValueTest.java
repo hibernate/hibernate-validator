9afd5ee846fca7838c5f9e7ae5336b7237a5c855
George Gastaldi <gegastaldi@gmail.com>
2015-10-14 13:01:15 -0300

    HV-1022: Missing check for type argument constraints on validateValue
    Type argument constraints should be checked before returning failing constraints

---
+++ engine-jdk8-tests/src/test/java/org/hibernate/validator/test/internal/engine/valuehandling/OptionalTypeAnnotationConstraintUsingValidateValueTest.java
@@ -0,0 +1,92 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.test.internal.engine.valuehandling;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.Optional;
+import java.util.Set;
+import javax.validation.Constraint;
+import javax.validation.ConstraintViolation;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.Validator;
+import javax.validation.constraints.Null;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.testutil.TestForIssue;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertCorrectConstraintTypes;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertCorrectConstraintViolationMessages;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertCorrectPropertyPaths;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertNumberOfViolations;
+import static org.hibernate.validator.testutil.ValidatorUtil.getValidator;
+
+/**
+ * Test usage of {@link Optional} on fields using validate value.
+ *
+ * @author George Gastaldi
+ */
+public class OptionalTypeAnnotationConstraintUsingValidateValueTest {
+	private Validator validator;
+
+	@BeforeClass
+	public void setup() {
+		validator = getValidator();
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HV-1022")
+	public void type_annotation_constraint_violation_is_propagated_even_if_there_are_no_other_constraints() {
+		Model model = new Model();
+		model.valueWithNullOrNotBlank = Optional.of( "" );
+
+		Set<ConstraintViolation<Model>> constraintViolations = validator.validateValue(
+				Model.class,
+				"valueWithNullOrNotBlank",
+				model.valueWithNullOrNotBlank
+		);
+		assertNumberOfViolations( constraintViolations, 1 );
+		assertCorrectPropertyPaths( constraintViolations, "valueWithNullOrNotBlank" );
+		assertCorrectConstraintViolationMessages( constraintViolations, "type" );
+		assertCorrectConstraintTypes( constraintViolations, NullOrNotBlank.class );
+	}
+
+	@ConstraintComposition(CompositionType.OR)
+	@Null
+	@NotBlank
+	@ReportAsSingleViolation
+	@Constraint(validatedBy = {})
+	@Target({ TYPE_USE, METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+	@Retention(RUNTIME)
+	@Documented
+	public @interface NullOrNotBlank {
+
+		String message() default "NullOrNotBlank";
+
+		Class<?>[] groups() default {};
+
+		Class<? extends Payload>[] payload() default {};
+	}
+
+	private static class Model {
+		Optional<@NullOrNotBlank(message = "type") String> valueWithNullOrNotBlank;
+	}
+}
