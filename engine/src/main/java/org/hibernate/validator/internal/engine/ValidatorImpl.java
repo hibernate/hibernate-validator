989689d578fb5db65395a4503e033698cbc1d330
Jan-Willem Willebrands <jwillebrands@gmail.com>
2016-06-21 21:34:50 +0200

    HV-1072 Determine number of failed constraints using context for return value.

--- engine/src/main/java/org/hibernate/validator/internal/engine/ValidatorImpl.java
+++ engine/src/main/java/org/hibernate/validator/internal/engine/ValidatorImpl.java
@@ -1343,11 +1343,9 @@ public class ValidatorImpl implements Validator, ExecutableValidator {
 		while ( sequenceIterator.hasNext() ) {
 			Sequence sequence = sequenceIterator.next();
 			for ( GroupWithInheritance groupOfGroups : sequence ) {
-				int numberOfFailingConstraint = 0;
+				int numberOfFailingConstraintsBeforeGroup = context.getFailingConstraints().size();
 				for ( Group group : groupOfGroups ) {
-					numberOfFailingConstraint += validateReturnValueForGroup(
-							context, bean, value, group
-					);
+					validateReturnValueForGroup( context, bean, value, group );
 					if ( shouldFailFast( context ) ) {
 						return;
 					}
@@ -1362,7 +1360,7 @@ public class ValidatorImpl implements Validator, ExecutableValidator {
 					}
 				}
 
-				if ( numberOfFailingConstraint > 0 ) {
+				if ( context.getFailingConstraints().size() > numberOfFailingConstraintsBeforeGroup ) {
 					break;
 				}
 			}
