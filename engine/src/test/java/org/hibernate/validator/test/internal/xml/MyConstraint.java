de4ddcfe3a3875344ebcc8adec87fc7bdb58d613
Guillaume Smet <guillaume.smet@gmail.com>
2016-09-16 17:13:05 +0200

    HV-1101 Fix a few infelicities in the XML parsing of the mapping configuration

---
+++ engine/src/test/java/org/hibernate/validator/test/internal/xml/MyConstraint.java
@@ -0,0 +1,41 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.test.internal.xml;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = MyConstraintValidator.class)
+@Target({ TYPE, METHOD, FIELD })
+@Retention(RUNTIME)
+public @interface MyConstraint {
+
+	String message() default "MyConstraint is not valid";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	AdditionalConstraint[] additionalConstraints() default {};
+
+	public @interface AdditionalConstraint {
+
+		String message() default "AdditionalConstraint is not valid";
+
+		String constraint();
+	}
+}
