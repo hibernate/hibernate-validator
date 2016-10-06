30a2613b86f1268e88216e048e6f5bba4e92dcf4
marko.bekhta <markob@prykladna.lviv.ua>
2016-10-06 11:17:52 +0200

    HV-1119 added checks for parameters and corresponding test cases

--- annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/annotationparameters/InvalidSizeParameters.java
+++ annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/annotationparameters/InvalidSizeParameters.java
@@ -6,24 +6,44 @@
  */
 package org.hibernate.validator.ap.testmodel.annotationparameters;
 
-import javax.validation.constraints.Size;
 import java.util.Collection;
+import javax.validation.constraints.Size;
 
 /**
  * @author Marko Bekhta
  */
 public class InvalidSizeParameters {
 
-	@Size( min = -1 )
+	@Size(min = -1)
 	private Collection<String> strings1;
 
-	@Size( max = -10 )
+	@Size(max = -10)
 	private Collection<String> strings2;
 
-	@Size( min = 15, max = 10 )
+	@Size(min = 15, max = 10)
 	private Collection<String> strings3;
 
 	@Size.List({ @Size(min = -1), @Size(max = -15), @Size(min = 15, max = 10) })
 	private Collection<String> strings4;
 
+	public InvalidSizeParameters(
+			@Size(min = -1) Collection<String> strings1,
+			@Size(max = -10) Collection<String> strings2,
+			@Size(min = 15, max = 10) Collection<String> strings3
+	) {
+
+	}
+
+	public void doSomething(
+			@Size(min = -1) Collection<String> strings1,
+			@Size(max = -10) Collection<String> strings2,
+			@Size(min = 15, max = 10) Collection<String> strings3
+	) {
+
+	}
+
+	@Size(min = -10)
+	public Collection<String> doSomething() {
+		return java.util.Collections.emptyList();
+	}
 }
--- annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/annotationparameters/ValidLengthParameters.java
+++ annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/annotationparameters/ValidLengthParameters.java
@@ -16,16 +16,28 @@ public class ValidLengthParameters {
 	@Length
 	private String string1;
 
-	@Length( min = 10 )
+	@Length(min = 10)
 	private String string2;
 
-	@Length( max = 10 )
+	@Length(max = 10)
 	private String string3;
 
-	@Length( min = 10, max = 15 )
+	@Length(min = 10, max = 15)
 	private String string4;
 
 	@Length.List({ @Length(min = 10, max = 15), @Length(max = 10), @Length(min = 10), @Length })
 	private String string5;
 
+	public ValidLengthParameters(@Length(min = 10) String string) {
+
+	}
+
+	public void doSomething(@Length(min = 10) String string) {
+
+	}
+
+	@Length(min = 10)
+	public String doSomething() {
+		return "";
+	}
 }
