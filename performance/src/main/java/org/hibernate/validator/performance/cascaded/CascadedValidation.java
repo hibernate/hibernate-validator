512b09938bd10ab434d9159f465721af80a67b42
marko-bekhta <markob@prykladna.lviv.ua>
2016-10-13 21:04:26 +0200

    HV-962 updated 5.3 validator version to final. cleaned up documentation.

--- performance/src/main/java/org/hibernate/validator/performance/cascaded/CascadedValidation.java
+++ performance/src/main/java/org/hibernate/validator/performance/cascaded/CascadedValidation.java
@@ -6,15 +6,9 @@
  */
 package org.hibernate.validator.performance.cascaded;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
 import javax.validation.Validation;
@@ -33,11 +27,13 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author Hardy Ferentschik
  */
 public class CascadedValidation {
-	private static final int NUMBER_OF_RUNNABLES = 10000;
+	private static final int NUMBER_OF_VALIDATION_ITERATIONS = 1000;
 	private static final int SIZE_OF_THREAD_POOL = 50;
 
 	@State(Scope.Benchmark)
@@ -71,23 +67,17 @@ public class CascadedValidation {
 		assertThat( violations ).hasSize( 0 );
 	}
 
-//	@Benchmark
-//	@BenchmarkMode(Mode.All)
-//	@OutputTimeUnit(TimeUnit.MILLISECONDS)
-//	@Fork(value = 1)
-//	@Threads(2)
-//	@Warmup(iterations = 10)
-//	@Measurement(iterations = 50)
-	public void testCascadedValidationMultiThreaded(CascadedValidationState state) throws Exception {
-		CountDownLatch startLatch = new CountDownLatch( 1 );
-		ExecutorService executor = Executors.newFixedThreadPool( SIZE_OF_THREAD_POOL );
-		for ( int i = 0; i <= NUMBER_OF_RUNNABLES; i++ ) {
-			Runnable run = new TestRunner( startLatch, state );
-			executor.execute( run );
+	@Benchmark
+	@BenchmarkMode(Mode.All)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Fork(value = 1)
+	@Threads(SIZE_OF_THREAD_POOL)
+	@Warmup(iterations = 10)
+	@Measurement(iterations = 50)
+	public void testCascadedValidationIterative(CascadedValidationState state) throws Exception {
+		for ( int i = 0; i < NUMBER_OF_VALIDATION_ITERATIONS; i++ ) {
+			testCascadedValidation( state );
 		}
-		executor.shutdown();
-		startLatch.countDown(); //start!
-		executor.awaitTermination( 600, TimeUnit.SECONDS );
 	}
 
 	public class Person {
@@ -104,31 +94,6 @@ public class CascadedValidation {
 		public Person addFriend(Person friend) {
 			friends.add( friend );
 			return this;
-		}
-	}
-
-	public class TestRunner implements Runnable {
-		private static final int NUMBER_OF_VALIDATION_ITERATIONS = 1000;
-		private final CountDownLatch latch;
-		private final CascadedValidationState state;
-
-		public TestRunner(CountDownLatch latch, CascadedValidationState state) {
-			this.latch = latch;
-			this.state = state;
-		}
-
-		@Override
-		public void run() {
-			try {
-				latch.await();
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-				return;
-			}
-			for ( int i = 0; i < NUMBER_OF_VALIDATION_ITERATIONS; i++ ) {
-				testCascadedValidation( state );
-			}
 		}
 	}
 }
