db152677e96503113134954100cb0a0d64972c76
marko.bekhta <markob@prykladna.lviv.ua>
2016-10-14 12:40:48 +0200

    HV-991 added more test cases and fixed a verification of '0' digit at the end

--- engine/src/test/java/org/hibernate/validator/test/constraints/pl/NIPValidatorTest.java
+++ engine/src/test/java/org/hibernate/validator/test/constraints/pl/NIPValidatorTest.java
@@ -18,6 +18,7 @@ import org.testng.annotations.Test;
 
 /**
  * Tests for NIP constraints.
+ * A generator which can be used to create fake numbers - <a href="http://www.bogus.ovh.org/generatory/all.html>generator</a>
  *
  * @author Marko Bekhta
  */
@@ -34,6 +35,14 @@ public class NIPValidatorTest {
 		assertNumberOfViolations( validator.validate( new Person( "123-456-78-19" ) ), 0 );
 		assertNumberOfViolations( validator.validate( new Person( "123-45-67-819" ) ), 0 );
 		assertNumberOfViolations( validator.validate( new Person( "123-456-32-18" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "5931423811" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "2596048500" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "4163450312" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "1786052059" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "6660057854" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "4219220786" ) ), 0 );
+		assertNumberOfViolations( validator.validate( new Person( "3497264632" ) ), 0 );
+
 	}
 
 	@Test
@@ -41,6 +50,13 @@ public class NIPValidatorTest {
 		assertNumberOfViolations( validator.validate( new Person( "123-456-78-14" ) ), 1 );
 		assertNumberOfViolations( validator.validate( new Person( "123-45-67-812" ) ), 1 );
 		assertNumberOfViolations( validator.validate( new Person( "123-456-32-12" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "5931423812" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "2596048505" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "4163450311" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "1786052053" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "6660057852" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "4219220785" ) ), 1 );
+		assertNumberOfViolations( validator.validate( new Person( "3497264639" ) ), 1 );
 	}
 
 
