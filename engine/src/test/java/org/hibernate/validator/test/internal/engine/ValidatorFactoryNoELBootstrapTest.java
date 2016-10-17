2042102dd76c0126a1f7450436adee1373c06446
Gunnar Morling <gunnar.morling@googlemail.com>
2016-10-17 12:22:15 +0200

    HV-1131 Implementing test routines without reflection

--- engine/src/test/java/org/hibernate/validator/test/internal/engine/ValidatorFactoryNoELBootstrapTest.java
+++ engine/src/test/java/org/hibernate/validator/test/internal/engine/ValidatorFactoryNoELBootstrapTest.java
@@ -6,6 +6,8 @@
  */
 package org.hibernate.validator.test.internal.engine;
 
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertCorrectConstraintViolationMessages;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -13,11 +15,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Set;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
-import javax.validation.bootstrap.GenericBootstrap;
+import javax.validation.Validator;
+import javax.validation.constraints.Min;
 
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.hibernate.validator.testutil.TestForIssue;
 import org.testng.annotations.Test;
 
@@ -29,38 +34,52 @@ public class ValidatorFactoryNoELBootstrapTest {
 
 	@Test
 	@TestForIssue(jiraKey = "HV-793")
-	public void testMissingELDependencyThrowsExceptionDuringFactoryBootstrap() throws Exception {
-		ClassLoader classLoader = new ELIgnoringClassLoader();
-		Class<?> clazz = classLoader.loadClass( Validation.class.getName() );
+	public void bootstrapFailsWhenUsingDefaultInterpolatorWithoutExpressionFactory() throws Throwable {
+		runWithoutElLibs( BootstrapFailsWhenUsingDefaultInterpolatorWithoutExpressionFactory.class );
+	}
 
-		Object validation = clazz.newInstance();
-		Method m = clazz.getMethod( "buildDefaultValidatorFactory" );
+	public static class BootstrapFailsWhenUsingDefaultInterpolatorWithoutExpressionFactory {
 
-		try {
-			m.invoke( validation );
-			fail( "An exception should have been thrown" );
-		}
-		catch (InvocationTargetException e) {
-			assertTrue(
-					getRootCause( e ).getMessage().startsWith( "HV000183" ),
-					"Bootstrapping in Validation should throw an unexpected exception: " + e.getMessage()
-			);
+		public void run() {
+			try {
+				Validation.buildDefaultValidatorFactory();
+				fail( "An exception should have been thrown" );
+			}
+			catch (Throwable e) {
+				assertTrue(
+						getRootCause( e ).getMessage().startsWith( "HV000183" ),
+						"Bootstrapping in Validation should throw an unexpected exception: " + e.getMessage()
+				);
+			}
 		}
 	}
 
 	@Test
 	@TestForIssue(jiraKey = "HV-1131")
-	public void testMissingELDependencyDontThrowExceptionDuringConfigurationInitialization() throws Exception {
-		ClassLoader classLoader = new ELIgnoringClassLoader();
+	public void canUseParameterInterpolatorWithoutExpressionFactory() throws Throwable {
+		runWithoutElLibs( CanUseParameterInterpolatorWithoutExpressionFactory.class );
+	}
 
-		Class<?> validationClass = classLoader.loadClass( Validation.class.getName() );
-		Method byDefaultProviderMethod = validationClass.getMethod( "byDefaultProvider" );
+	public static class CanUseParameterInterpolatorWithoutExpressionFactory {
 
-		Class<?> genericBootstrapClass = classLoader.loadClass( GenericBootstrap.class.getName() );
-		Method configureMethod = genericBootstrapClass.getMethod( "configure" );
+		public void run() {
+			Validator validator = Validation.byDefaultProvider()
+				.configure()
+				.messageInterpolator( new ParameterMessageInterpolator() )
+				.buildValidatorFactory()
+				.getValidator();
 
-		Object genericBootstrap = byDefaultProviderMethod.invoke( null );
-		configureMethod.invoke( genericBootstrap );
+			assertNotNull( validator );
+
+			Set<ConstraintViolation<SomeBean>> violations = validator.validate( new SomeBean() );
+			assertCorrectConstraintViolationMessages( violations, "must be greater than or equal to 42" );
+		}
+	}
+
+	public static class SomeBean {
+
+		@Min(42)
+		private final long myLong = 41;
 	}
 
 	/**
@@ -68,7 +87,7 @@ public class ValidatorFactoryNoELBootstrapTest {
 	 * To test what happens if the EL classes cannot be found, we need to somehow "remove" these classes from the
 	 * classpath.
 	 */
-	public class ELIgnoringClassLoader extends ClassLoader {
+	private static class ELIgnoringClassLoader extends ClassLoader {
 		private final String EL_PACKAGE_PREFIX = "javax.el";
 		private final String[] PASS_THROUGH_PACKAGE_PREFIXES = new String[] {
 				"java.",
@@ -124,39 +143,52 @@ public class ValidatorFactoryNoELBootstrapTest {
 
 			return clazz;
 		}
+
+		private String getPackageName(String className) {
+			int i = className.lastIndexOf( '.' );
+			if ( i > 0 ) {
+				return className.substring( 0, i );
+			}
+			else {
+				return null;
+			}
+		}
+
+		private byte[] loadClassData(String className) throws IOException, ClassNotFoundException {
+			String path = "/" + className.replace( ".", "/" ) + ".class";
+			InputStream in = ValidatorFactoryNoELBootstrapTest.class.getResourceAsStream( path );
+
+			if ( in == null ) {
+				throw new ClassNotFoundException();
+			}
+
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+			int bytesRead;
+			byte[] data = new byte[16384];
+			while ( ( bytesRead = in.read( data, 0, data.length ) ) != -1 ) {
+				buffer.write( data, 0, bytesRead );
+			}
+
+			buffer.flush();
+			return buffer.toByteArray();
+		}
 	}
 
-	private static String getPackageName(String className) {
-		int i = className.lastIndexOf( '.' );
-		if ( i > 0 ) {
-			return className.substring( 0, i );
+	/**
+	 * Loads the given class using the EL-ignoring class loader and executes it.
+	 */
+	private void runWithoutElLibs(Class<?> delegateType) throws Throwable {
+		try {
+			Object test = new ELIgnoringClassLoader().loadClass( delegateType.getName() ).newInstance();
+			test.getClass().getMethod( "run" ).invoke( test );
 		}
-		else {
-			return null;
+		catch (InvocationTargetException ite) {
+			throw ite.getCause();
 		}
 	}
 
-	private byte[] loadClassData(String className) throws IOException, ClassNotFoundException {
-		String path = "/" + className.replace( ".", "/" ) + ".class";
-		InputStream in = ValidatorFactoryNoELBootstrapTest.class.getResourceAsStream( path );
-
-		if ( in == null ) {
-			throw new ClassNotFoundException();
-		}
-
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-		int bytesRead;
-		byte[] data = new byte[16384];
-		while ( ( bytesRead = in.read( data, 0, data.length ) ) != -1 ) {
-			buffer.write( data, 0, bytesRead );
-		}
-
-		buffer.flush();
-		return buffer.toByteArray();
-	}
-
-	private Throwable getRootCause(Throwable throwable) {
+	private static Throwable getRootCause(Throwable throwable) {
 		while ( true ) {
 			Throwable cause = throwable.getCause();
 			if ( cause == null ) {
