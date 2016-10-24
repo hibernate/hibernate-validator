10cdc97a0c4c022a61ae3a8899a5b16580052fc5
Gunnar Morling <gunnar.morling@googlemail.com>
2016-10-24 09:45:37 +0200

    HV-991 Some JavaDoc and formatting improvements

--- engine/src/main/java/org/hibernate/validator/constraints/pl/NIP.java
+++ engine/src/main/java/org/hibernate/validator/constraints/pl/NIP.java
@@ -18,18 +18,17 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
 
-
 /**
- * Validates a NIP number - (9 digit number Polish VAT identification number ).
+ * Validates that a {@code CharSequence} is a NIP number (9-digit Polish VAT identification number).
  *
  * @author Marko Bekhta
  * @see <a href="https://pl.wikipedia.org/wiki/NIP">NIP</a> }
  */
-
 @Documented
 @Constraint(validatedBy = { })
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
