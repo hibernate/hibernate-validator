b0804e7c829a06b5807e9c3ba937194fcc46726a
jmaxwell <jmaxwell@theice.com>
2016-12-12 13:06:40 -0600

    HV-1184 add synthetic check to ConstraintDescriptorImpl.buildAnnotationParameterMap
    Addresses issue with jacoco

--- engine/src/main/java/org/hibernate/validator/internal/metadata/descriptor/ConstraintDescriptorImpl.java
+++ engine/src/main/java/org/hibernate/validator/internal/metadata/descriptor/ConstraintDescriptorImpl.java
@@ -557,8 +557,11 @@ public class ConstraintDescriptorImpl<T extends Annotation> implements Constrain
 		final Method[] declaredMethods = run( GetDeclaredMethods.action( annotation.annotationType() ) );
 		Map<String, Object> parameters = newHashMap( declaredMethods.length );
 		for ( Method m : declaredMethods ) {
-			Object value = run( GetAnnotationParameter.action( annotation, m.getName(), Object.class ) );
-			parameters.put( m.getName(), value );
+		    // Add check for synthetic
+            if (!m.isSynthetic()) {
+                Object value = run(GetAnnotationParameter.action(annotation, m.getName(), Object.class));
+                parameters.put(m.getName(), value);
+            }
 		}
 		return Collections.unmodifiableMap( parameters );
 	}
