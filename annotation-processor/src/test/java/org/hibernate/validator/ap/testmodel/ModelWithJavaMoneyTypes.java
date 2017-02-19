dd338b0fd72aefe29185c23961b9c591f344bba1
Guillaume Husta <g.husta@free.fr>
2017-02-19 23:52:15 +0100

    HV-1252 Adding comprehensive constraints annotations usable with MonetaryAmount (tests)

--- annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/ModelWithJavaMoneyTypes.java
+++ annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/ModelWithJavaMoneyTypes.java
@@ -5,6 +5,8 @@ import org.hibernate.validator.constraints.Currency;
 import javax.money.MonetaryAmount;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  * Constraint @{@link org.hibernate.validator.constraints.Currency},
@@ -22,5 +24,9 @@ public class ModelWithJavaMoneyTypes
     @DecimalMax("1000.00")
     @DecimalMin("0.00")
     public MonetaryAmount monetaryAmount;
+
+    @Max(1000L)
+    @Min(1L)
+    public MonetaryAmount anotherMonetaryAmount;
 
 }
