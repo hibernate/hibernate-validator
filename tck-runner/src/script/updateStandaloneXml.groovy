bc0c9e9ae441a7aa327f9e1b661122f96ac3eea4
Guillaume Smet <guillaume.smet@gmail.com>
2017-06-07 14:29:01 +0200

    HV-1354 Make JavaFX classes visible when running the TCK in incontainer mode

---
+++ tck-runner/src/script/updateStandaloneXml.groovy
@@ -0,0 +1,18 @@
+println "[INFO] ------------------------------------------------------------------------";
+println "[INFO] UPDATING standalone.xml                                                 ";
+println "[INFO] ------------------------------------------------------------------------";
+
+def processFileInplace(File file, Closure processText) {
+    def text = file.text
+    file.write( processText( text ) )
+}
+
+// Add javafx.api module to the global modules
+standaloneXml = new File( project.properties['wildflyTargetDir'], 'standalone/configuration/standalone.xml' )
+println "[INFO] Add javafx.api as global module"
+
+processFileInplace( standaloneXml ) { text ->
+    text.replaceAll( /<subsystem xmlns="urn:jboss:domain:ee:4\.0">/, '<subsystem xmlns="urn:jboss:domain:ee:4.0">\n            <global-modules>\n                <module name="javafx.api" slot="main" />\n            </global-modules>' )
+}
+
+println "[INFO] ------------------------------------------------------------------------";
