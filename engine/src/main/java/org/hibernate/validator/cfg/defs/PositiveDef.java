4f8162744d5d22c0d7d0ee670c2bd4f0aca55f61
Gunnar Morling <gunnar.morling@googlemail.com>
2017-06-16 15:49:00 +0200

    HV-1372 Adding def types for @Positive etc.

---
+++ engine/src/main/java/org/hibernate/validator/cfg/defs/PositiveDef.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.cfg.defs;
+
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.cfg.ConstraintDef;
+
+/**
+ * @author Gunnar Morling
+ */
+public class PositiveDef extends ConstraintDef<PositiveDef, Positive> {
+
+	public PositiveDef() {
+		super( Positive.class );
+	}
+}
