03c215063d2461bdc58f52d80a9e43609333420b
Gunnar Morling <gunnar.morling@googlemail.com>
2017-06-16 15:49:00 +0200

    HV-1369 Adding def types for @Positive etc.

---
+++ engine/src/main/java/org/hibernate/validator/cfg/defs/NegativeOrZeroDef.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.cfg.defs;
+
+import javax.validation.constraints.NegativeOrZero;
+
+import org.hibernate.validator.cfg.ConstraintDef;
+
+/**
+ * @author Gunnar Morling
+ */
+public class NegativeOrZeroDef extends ConstraintDef<NegativeOrZeroDef, NegativeOrZero> {
+
+	public NegativeOrZeroDef() {
+		super( NegativeOrZero.class );
+	}
+}
