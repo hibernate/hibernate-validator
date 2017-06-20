d86a2301402a04352b913b6a65f1b3f4b82a0c72
marko-bekhta <markob@prykladna.lviv.ua>
2017-06-20 20:26:52 +0200

    HV-1377 Adding a check if the type was already processed

---
+++ annotation-processor/src/test/java/org/hibernate/validator/ap/testmodel/circular/CircularProperty.java
@@ -0,0 +1,20 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.ap.testmodel.circular;
+
+/**
+ * @author Marko Bekhta
+ */
+public interface CircularProperty {
+
+	void doSomething();
+
+	interface ImprovedCircularProperty extends CircularProperty {
+
+		void maybeDoSomething();
+	}
+}
