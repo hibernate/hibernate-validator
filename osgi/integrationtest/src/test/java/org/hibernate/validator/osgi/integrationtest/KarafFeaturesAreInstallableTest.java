fb160e85fe86324261bd77965d5b80c27076daac
Toni Menzel <toni.menzel@rebaze.com>
2017-07-07 14:30:15 +0200

    Adds new tests installing the karaf features on-demand
    This commit adds a new Pax Exam based test installing both karaf features provided by this project on a vanilla karaf.
    It highlights the issue found by https://github.com/hibernate/hibernate-validator/pull/803 but should be extended whenever a new feature is being added to the repo.

---
+++ osgi/integrationtest/src/test/java/org/hibernate/validator/osgi/integrationtest/KarafFeaturesAreInstallableTest.java
@@ -0,0 +1,114 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.osgi.integrationtest;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Locale;
+import javax.inject.Inject;
+import org.apache.karaf.features.Feature;
+import org.apache.karaf.features.FeaturesService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.when;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.debugConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+
+/**
+ * Integration test for Bean Validation and Hibernate Validator under OSGi.
+ * <p>
+ * This test makes sure that the karaf features provided by this project are installable.
+ *
+ * @author Toni Menzel (toni@rebaze.com)
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class KarafFeaturesAreInstallableTest {
+
+	@Inject
+	protected FeaturesService featuresService;
+
+	private static final boolean DEBUG = false;
+
+	@Configuration
+	public Option[] config() {
+		MavenArtifactUrlReference hibernateValidatorFeature = maven()
+				.groupId( "org.hibernate.validator" )
+				.artifactId( "hibernate-validator-osgi-karaf-features" )
+				.classifier( "features" )
+				.type( "xml" )
+				.versionAsInProject();
+
+		return options(
+				when( DEBUG ).useOptions( debugConfiguration( "5005", true ) ),
+				karafDistributionConfiguration()
+						.frameworkUrl(
+								maven()
+										.groupId( "org.apache.karaf" )
+										.artifactId( "apache-karaf" )
+										.type( "tar.gz" )
+										.versionAsInProject()
+						)
+						.unpackDirectory( new File( "target/exam" ) )
+						.useDeployFolder( false ),
+				configureConsole()
+						.ignoreLocalConsole()
+						.ignoreRemoteShell(),
+				when( DEBUG ).useOptions( keepRuntimeFolder() ),
+				logLevel( LogLevelOption.LogLevel.INFO ),
+				// avoiding additional boot features; specifically "enterprise" which already comes with a HV feature
+				// "system" is the absolute minimum, but enough for our purposes
+				editConfigurationFilePut(
+						"etc/org.apache.karaf.features.cfg",
+						"featuresBoot",
+						"system"
+				),
+				systemProperty( "validatorRepositoryUrl" ).value( hibernateValidatorFeature.getURL() )
+		);
+	}
+
+	@BeforeClass
+	public static void setLocaleToEnglish() {
+		Locale.setDefault( Locale.ENGLISH );
+	}
+
+	@Test
+	public void canInstallFeatureHibernateValidator() throws Exception {
+		featuresService.addRepository( new URI( System.getProperty( "validatorRepositoryUrl" ) ) );
+		canInstallFeature( "hibernate-validator" );
+	}
+
+	@Test
+	public void canInstallFeatureHibernateValidatorParanamer() throws Exception {
+		featuresService.addRepository( new URI( System.getProperty( "validatorRepositoryUrl" ) ) );
+		canInstallFeature( "hibernate-validator-paranamer" );
+	}
+
+	public void canInstallFeature(String featureName) throws Exception {
+		Feature feature = featuresService.getFeature( featureName );
+		assertTrue( featureName + " feature is not available from features list", feature != null );
+		featuresService.installFeature( featureName );
+		assertTrue( featureName + " feature isn't installed, though available from features list", featuresService.isInstalled( feature ) );
+	}
+}
