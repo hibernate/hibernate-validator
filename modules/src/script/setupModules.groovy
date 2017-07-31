8db9744c48437ecc89d01becd4237583302f0ccd
Guillaume Smet <guillaume.smet@gmail.com>
2017-07-31 16:50:06 +0200

    HV-1440 Provide patch file for WF 11.x

--- modules/src/script/setupModules.groovy
+++ modules/src/script/setupModules.groovy
@@ -7,22 +7,28 @@ def processFileInplace(File file, Closure processText) {
     file.write( processText( text ) )
 }
 
+def deleteFiles(List<String> filePaths) {
+    for ( String filePath : filePaths ) {
+        new File( filePath ).delete();
+    }
+}
+
 def appendDependency(File file, String dependencyToAppend, boolean optional) {
     file.write( file.text.replaceAll( /<\/dependencies>/, '  <module name="' + dependencyToAppend + '"' + ( optional ? ' optional="true"' : '' ) + '/>\n  </dependencies>' ) )
 }
 
 // BV API
-bvModuleXml = new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/javax/validation/api/main/module.xml' )
+bvModuleXml = new File( wildflyPatchedTargetDir, 'modules/system/layers/base/javax/validation/api/main/module.xml' )
 def bvArtifactName = 'validation-api-' + project.properties['bv.api.version'] + '.jar';
 println "[INFO] Using BV version " + bvArtifactName;
 processFileInplace( bvModuleXml ) { text ->
     text.replaceAll( /validation-api.*jar/, bvArtifactName )
 }
 
-new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/javax/validation/api/main/validation-api-1.1.0.Final.jar' ).delete()
+deleteFiles( new FileNameFinder().getFileNames( wildflyPatchedTargetDir + '/modules/system/layers/base/javax/validation/api/main', 'validation-api-*.jar' ) )
 
 // HV
-hvModuleXml = new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/org/hibernate/validator/main/module.xml' )
+hvModuleXml = new File( wildflyPatchedTargetDir, 'modules/system/layers/base/org/hibernate/validator/main/module.xml' )
 def hvArtifactName = 'hibernate-validator-' + project.version + '.jar';
 println "[INFO] Using HV version " + hvArtifactName;
 processFileInplace( hvModuleXml ) { text ->
@@ -31,16 +37,16 @@ processFileInplace( hvModuleXml ) { text ->
 appendDependency( hvModuleXml, "javax.money.api", true )
 appendDependency( hvModuleXml, "javafx.api", true )
 
-new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/org/hibernate/validator/main/hibernate-validator-5.2.4.Final.jar' ).delete()
+deleteFiles( new FileNameFinder().getFileNames( wildflyPatchedTargetDir + '/modules/system/layers/base/org/hibernate/validator/main', 'hibernate-validator-*.jar' ) )
 
 // HV CDI
-hvCdiModuleXml = new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/org/hibernate/validator/cdi/main/module.xml' )
+hvCdiModuleXml = new File( wildflyPatchedTargetDir, 'modules/system/layers/base/org/hibernate/validator/cdi/main/module.xml' )
 def hvCdiArtifactName = 'hibernate-validator-cdi-' + project.version + '.jar';
 println "[INFO] Using HV CDI Portable Extension version " + hvCdiArtifactName;
 processFileInplace( hvCdiModuleXml ) { text ->
     text.replaceAll( /hibernate-validator-cdi.*jar/, hvCdiArtifactName )
 }
 
-new File( project.properties['wildflyPatchedTargetDir'], 'modules/system/layers/base/org/hibernate/validator/cdi/main/hibernate-validator-cdi-5.2.4.Final.jar' ).delete()
+deleteFiles( new FileNameFinder().getFileNames( wildflyPatchedTargetDir + '/modules/system/layers/base/org/hibernate/validator/cdi/main', 'hibernate-validator-cdi-*.jar' ) )
 
 println "[INFO] ------------------------------------------------------------------------";
