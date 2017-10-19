0778a5c98b817771a645c6f4ba0b28dd8b5437b8
Guillaume Smet <guillaume.smet@gmail.com>
2017-10-19 15:38:27 +0200

    HV-1498 Fix privilege escalation when running under the security manager

--- engine/src/main/java/org/hibernate/validator/internal/util/privilegedactions/GetDeclaredField.java
+++ engine/src/main/java/org/hibernate/validator/internal/util/privilegedactions/GetDeclaredField.java
@@ -31,7 +31,6 @@ public final class GetDeclaredField implements PrivilegedAction<Field> {
 	public Field run() {
 		try {
 			final Field field = clazz.getDeclaredField( fieldName );
-			field.setAccessible( true );
 			return field;
 		}
 		catch (NoSuchFieldException e) {
