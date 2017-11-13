f0af5b0fbcac8f59695dcd3f0764a2afc2919214
Guillaume Smet <guillaume.smet@gmail.com>
2017-11-13 13:30:17 +0100

    HV-1466 Various adjustments

--- engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/hv/UniqueElementsValidator.java
+++ engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/hv/UniqueElementsValidator.java
@@ -6,64 +6,61 @@
  */
 package org.hibernate.validator.internal.constraintvalidators.hv;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.hibernate.validator.constraints.UniqueElements;
 import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
-import org.hibernate.validator.internal.engine.messageinterpolation.util.InterpolationHelper;
-
-import static java.util.stream.Collectors.toList;
+import org.hibernate.validator.internal.util.CollectionHelper;
 
 /**
- * Validates that the provided collection contains unique elements, that no two elements in the collection are equal.
+ * Validates that the provided collection only contains unique elements, i.e. that we can't find 2 equal elements in the
+ * collection.
  * <p>
- * Uniqueness is defined by the equals method of the objects being compared.
+ * Uniqueness is defined by the {@code equals()} method of the objects being compared.
  *
  * @author Tadhg Pearson
+ * @author Guillaume Smet
  */
 public class UniqueElementsValidator implements ConstraintValidator<UniqueElements, Collection<?>> {
 
 	/**
-	 * @param objects Potentially null collection of input objects to check for duplicates
-	 * @param validatorContext Non-null {@link ConstraintValidatorContext} to accumulate validation messages
+	 * @param collection the collection to validate
+	 * @param constraintValidatorContext context in which the constraint is evaluated
 	 *
-	 * @return True if the input collection is null or does not contain duplicate objects
+	 * @return true if the input collection is null or does not contain duplicate elements
 	 */
 	@Override
-	public boolean isValid(Collection<?> objects, ConstraintValidatorContext validatorContext) {
-		boolean valid = isValid( objects );
-		if ( !valid ) {
-			List<String> duplicateElements = findDuplicates( objects );
-			String duplicates = InterpolationHelper.escapeMessageParameter( duplicateElements.toString() );
-
-			if ( validatorContext instanceof HibernateConstraintValidatorContext ) {
-				validatorContext.unwrap( HibernateConstraintValidatorContext.class )
-						.addMessageParameter( "duplicates", duplicates );
-			}
+	public boolean isValid(Collection<?> collection, ConstraintValidatorContext constraintValidatorContext) {
+		if ( collection == null || collection.size() < 2 ) {
+			return true;
 		}
-		return valid;
+
+		List<Object> duplicates = findDuplicates( collection );
+
+		if ( duplicates.isEmpty() ) {
+			return true;
+		}
+
+		if ( constraintValidatorContext instanceof HibernateConstraintValidatorContext ) {
+			constraintValidatorContext.unwrap( HibernateConstraintValidatorContext.class )
+					.addMessageParameter( "duplicates", duplicates.stream().map( String::valueOf ).collect( Collectors.joining( ", " ) ) )
+					.withDynamicPayload( CollectionHelper.toImmutableList( duplicates ) );
+		}
+
+		return false;
 	}
 
-	private boolean isValid(Collection<?> objects) {
-		boolean valid;
-		if ( objects == null ) {
-			valid = true;
-		}
-		else {
-			Set<?> set = new HashSet<>( objects );
-			valid = set.size() == objects.size();
-		}
-		return valid;
-	}
-
-
-	private List<String> findDuplicates(Collection<?> objects) {
-		Set<Object> uniqueElements = new HashSet<>();
-		return objects.stream().filter( o -> !uniqueElements.add( o ) ).map( String::valueOf ).collect( toList() );
+	private List<Object> findDuplicates(Collection<?> collection) {
+		Set<Object> uniqueElements = CollectionHelper.newHashSet( collection.size() );
+		return collection.stream().filter( o -> !uniqueElements.add( o ) )
+				.collect( toList() );
 	}
 }
