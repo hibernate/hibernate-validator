ee55769dd7dc265c35d492ab9f271c5e4db8c868
Guillaume Smet <guillaume.smet@gmail.com>
2017-11-14 15:35:02 +0100

    HV-1466 Adjust the targets of the @UniqueElements constraint

--- engine/src/main/java/org/hibernate/validator/constraints/UniqueElements.java
+++ engine/src/main/java/org/hibernate/validator/constraints/UniqueElements.java
@@ -6,8 +6,14 @@
  */
 package org.hibernate.validator.constraints;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -30,7 +36,7 @@ import org.hibernate.validator.internal.constraintvalidators.hv.UniqueElementsVa
  * @since 6.0.5
  */
 @Documented
-@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = UniqueElementsValidator.class)
 public @interface UniqueElements {
