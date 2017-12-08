d7146ae4625a3364682d5115dc9d1f30cb489603
Guillaume Smet <guillaume.smet@gmail.com>
2017-12-08 16:34:17 +0100

    HV-1537 Reintroduce ReflectionParameterNameProvider to avoid API breakage

---
+++ engine/src/main/java/org/hibernate/validator/parameternameprovider/ReflectionParameterNameProvider.java
@@ -0,0 +1,51 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.parameternameprovider;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+import javax.validation.ParameterNameProvider;
+
+/**
+ * Uses Java 8 reflection to get the parameter names.
+ *
+ * <p>For this provider to return the actual parameter names, classes must be compiled with the '-parameters' compiler
+ * argument. Otherwise, the JDK will return synthetic names in the form {@code arg0}, {@code arg1}, etc.</p>
+ *
+ * <p>See also <a href="http://openjdk.java.net/jeps/118">JEP 118</a></p>
+ * @author Khalid Alqinyah
+ *
+ * @since 5.2
+ */
+public class ReflectionParameterNameProvider implements ParameterNameProvider {
+
+	@Override
+	public List<String> getParameterNames(Constructor<?> constructor) {
+		return getParameterNames( constructor.getParameters() );
+	}
+
+	@Override
+	public List<String> getParameterNames(Method method) {
+		return getParameterNames( method.getParameters() );
+	}
+
+	private List<String> getParameterNames(Parameter[] parameters) {
+		List<String> parameterNames = newArrayList();
+
+		for ( Parameter parameter : parameters ) {
+			// If '-parameters' is used at compile time, actual names will be returned. Otherwise, it will be arg0, arg1...
+			parameterNames.add( parameter.getName() );
+		}
+
+		return parameterNames;
+	}
+}
