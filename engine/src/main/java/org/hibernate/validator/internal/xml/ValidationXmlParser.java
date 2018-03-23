f6020a8c5962a56b7579064c876ef9aee60f2995
marko-bekhta <marko.prykladna@gmail.com>
2018-03-23 16:54:45 +0100

    HV-1577 Added validation configuration stax parser

--- engine/src/main/java/org/hibernate/validator/internal/xml/ValidationXmlParser.java
+++ engine/src/main/java/org/hibernate/validator/internal/xml/ValidationXmlParser.java
@@ -11,38 +11,22 @@ import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.security.PrivilegedExceptionAction;
 import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.validation.BootstrapConfiguration;
-import javax.validation.executable.ExecutableType;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.Validator;
 
-import org.hibernate.validator.HibernateValidatorConfiguration;
 import org.hibernate.validator.internal.util.CollectionHelper;
 import org.hibernate.validator.internal.util.logging.Log;
 import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.hibernate.validator.internal.util.privilegedactions.GetClassLoader;
-import org.hibernate.validator.internal.util.privilegedactions.NewJaxbContext;
 import org.hibernate.validator.internal.util.privilegedactions.SetContextClassLoader;
-import org.hibernate.validator.internal.util.privilegedactions.Unmarshal;
-import org.hibernate.validator.internal.xml.binding.DefaultValidatedExecutableTypesType;
-import org.hibernate.validator.internal.xml.binding.ExecutableValidationType;
-import org.hibernate.validator.internal.xml.binding.PropertyType;
-import org.hibernate.validator.internal.xml.binding.ValidationConfigType;
+import org.hibernate.validator.internal.xml.stax.config.ValidationConfigStaxBuilder;
 
 import org.xml.sax.SAXException;
 
@@ -105,9 +89,8 @@ public class ValidationXmlParser {
 
 			in.reset();
 
-			// The validation is done first as we manipulate the XML document before pushing it to the unmarshaller
-			// and it might not be valid anymore as we might have switched the namespace to the latest namespace
-			// supported.
+			// The validation is done first as StAX builders used below are assuming that the XML file is correct and don't
+			// do any validation of the input.
 			Schema schema = getSchema( xmlParserHelper, schemaVersion );
 			Validator validator = schema.newValidator();
 			validator.validate( new StreamSource( new CloseIgnoringInputStream( in ) ) );
@@ -115,10 +98,13 @@ public class ValidationXmlParser {
 			in.reset();
 
 			xmlEventReader = xmlParserHelper.createXmlEventReader( VALIDATION_XML_FILE, new CloseIgnoringInputStream( in ) );
-			ValidationConfigType validationConfig = unmarshal( xmlEventReader );
-			xmlEventReader.close();
 
-			return createBootstrapConfiguration( validationConfig );
+			ValidationConfigStaxBuilder validationConfigStaxBuilder = new ValidationConfigStaxBuilder( xmlEventReader );
+
+			xmlEventReader.close();
+			in.reset();
+
+			return validationConfigStaxBuilder.build();
 		}
 		catch (XMLStreamException | IOException | SAXException e) {
 			throw LOG.getUnableToParseValidationXmlFileException( VALIDATION_XML_FILE, e );
@@ -152,25 +138,6 @@ public class ValidationXmlParser {
 		return xmlParserHelper.getSchema( schemaResource );
 	}
 
-	private ValidationConfigType unmarshal(XMLEventReader xmlEventReader) {
-		LOG.parsingXMLFile( VALIDATION_XML_FILE );
-
-		try {
-			// JAXBContext#newInstance() requires several permissions internally and doesn't use any privileged blocks
-			// itself; Wrapping it here avoids that all calling code bases need to have these permissions as well
-			JAXBContext jc = run( NewJaxbContext.action( ValidationConfigType.class ) );
-			Unmarshaller unmarshaller = jc.createUnmarshaller();
-
-			// Unmashaller#unmarshal() requires several permissions internally and doesn't use any privileged blocks
-			// itself; Wrapping it here avoids that all calling code bases need to have these permissions as well
-			JAXBElement<ValidationConfigType> root = run( Unmarshal.action( unmarshaller, xmlEventReader, ValidationConfigType.class ) );
-			return root.getValue();
-		}
-		catch (Exception e) {
-			throw LOG.getUnableToParseValidationXmlFileException( VALIDATION_XML_FILE, e );
-		}
-	}
-
 	private void closeStream(InputStream inputStream) {
 		try {
 			inputStream.close();
@@ -178,78 +145,6 @@ public class ValidationXmlParser {
 		catch (IOException io) {
 			LOG.unableToCloseXMLFileInputStream( VALIDATION_XML_FILE );
 		}
-	}
-
-	private BootstrapConfiguration createBootstrapConfiguration(ValidationConfigType config) {
-		Map<String, String> properties = new HashMap<>();
-		for ( PropertyType property : config.getProperty() ) {
-			if ( LOG.isDebugEnabled() ) {
-				LOG.debugf(
-						"Found property '%s' with value '%s' in validation.xml.",
-						property.getName(),
-						property.getValue()
-				);
-			}
-			properties.put( property.getName(), property.getValue() );
-		}
-
-		ExecutableValidationType executableValidationType = config.getExecutableValidation();
-		EnumSet<ExecutableType> defaultValidatedExecutableTypes = executableValidationType == null
-				? getValidatedExecutableTypes( null )
-				: getValidatedExecutableTypes( executableValidationType.getDefaultValidatedExecutableTypes() );
-		boolean executableValidationEnabled = executableValidationType == null || executableValidationType.getEnabled();
-
-		return new BootstrapConfigurationImpl(
-				config.getDefaultProvider(),
-				config.getConstraintValidatorFactory(),
-				config.getMessageInterpolator(),
-				config.getTraversableResolver(),
-				config.getParameterNameProvider(),
-				config.getClockProvider(),
-				getScriptEvaluatorFactoryClassProperty( config.getProperty() ),
-				getValueExtractorClassNames( config ),
-				defaultValidatedExecutableTypes,
-				executableValidationEnabled,
-				new HashSet<>( config.getConstraintMapping() ),
-				properties
-		);
-	}
-
-	private String getScriptEvaluatorFactoryClassProperty(List<PropertyType> properties) {
-		return properties.stream()
-				.filter( property -> HibernateValidatorConfiguration.SCRIPT_EVALUATOR_FACTORY_CLASSNAME.equals( property.getName() ) )
-				.map( PropertyType::getValue )
-				.findFirst().orElse( null );
-	}
-
-	private Set<String> getValueExtractorClassNames(ValidationConfigType config) {
-		Set<String> valueExtractorClassNames = CollectionHelper.newHashSet( config.getValueExtractor().size() );
-		for ( String className : config.getValueExtractor() ) {
-			if ( !valueExtractorClassNames.add( className ) ) {
-				throw LOG.getDuplicateDefinitionsOfValueExtractorException( className );
-			}
-		}
-		return valueExtractorClassNames;
-	}
-
-	/**
-	 * Returns an enum set with the executable types corresponding to the given
-	 * XML configuration, considering the special elements
-	 * {@link ExecutableType#ALL} and {@link ExecutableType#NONE}.
-	 *
-	 * @param validatedExecutables Schema type with executable types.
-	 *
-	 * @return An enum set representing the given executable types.
-	 */
-	private EnumSet<ExecutableType> getValidatedExecutableTypes(DefaultValidatedExecutableTypesType validatedExecutables) {
-		if ( validatedExecutables == null ) {
-			return null;
-		}
-
-		EnumSet<ExecutableType> executableTypes = EnumSet.noneOf( ExecutableType.class );
-		executableTypes.addAll( validatedExecutables.getExecutableType() );
-
-		return executableTypes;
 	}
 
 	/**
@@ -262,13 +157,4 @@ public class ValidationXmlParser {
 		return System.getSecurityManager() != null ? AccessController.doPrivileged( action ) : action.run();
 	}
 
-	/**
-	 * Runs the given privileged action, using a privileged block if required.
-	 * <p>
-	 * <b>NOTE:</b> This must never be changed into a publicly available method to avoid execution of arbitrary
-	 * privileged actions within HV's protection domain.
-	 */
-	private <T> T run(PrivilegedExceptionAction<T> action) throws Exception {
-		return System.getSecurityManager() != null ? AccessController.doPrivileged( action ) : action.run();
-	}
 }
