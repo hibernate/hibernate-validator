d20e4edbcdef5af36178c9b7c57a031ab022d3cf
Guillaume Smet <guillaume.smet@gmail.com>
2018-06-08 12:19:29 +0200

    HV-1628 Mark the classes using JavaFX as ignored by forbidden-apis

--- engine/src/main/java/org/hibernate/validator/internal/engine/valueextraction/SetPropertyValueExtractor.java
+++ engine/src/main/java/org/hibernate/validator/internal/engine/valueextraction/SetPropertyValueExtractor.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import javax.validation.valueextraction.ExtractedValue;
 import javax.validation.valueextraction.ValueExtractor;
 
+import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 import org.hibernate.validator.internal.engine.path.NodeImpl;
 
 import javafx.beans.property.SetProperty;
@@ -25,6 +26,7 @@ import javafx.beans.value.ObservableValue;
  * @author Guillaume Smet
  */
 @SuppressWarnings("restriction")
+@IgnoreForbiddenApisErrors(reason = "Usage of JavaFX classes")
 class SetPropertyValueExtractor implements ValueExtractor<SetProperty<@ExtractedValue ?>> {
 
 	static final ValueExtractorDescriptor DESCRIPTOR = new ValueExtractorDescriptor( new SetPropertyValueExtractor() );
