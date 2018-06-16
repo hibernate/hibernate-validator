da3502f7b762b0f4b11dd3085f96e4430a501d75
marko-bekhta <marko.prykladna@gmail.com>
2018-06-16 20:56:21 +0200

    HV-1363 Add documentation section on getter property selection strategy

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter12/getterselectionstrategy/GetterPropertySelectionStrategyTest.java
@@ -0,0 +1,51 @@
+package org.hibernate.validator.referenceguide.chapter12.getterselectionstrategy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.hibernate.validator.HibernateValidator;
+
+import org.junit.Test;
+
+public class GetterPropertySelectionStrategyTest {
+
+	@Test
+	public void defaultStrategyUsed() {
+		//tag::no-strategy[]
+		Validator validator = Validation.byProvider( HibernateValidator.class )
+				.configure()
+				.buildValidatorFactory()
+				.getValidator();
+
+		User user = new User( "", "", "not an email" );
+
+		Set<ConstraintViolation<User>> constraintViolations = validator.validate( user );
+
+		// as User has nonstandard getters no violations are triggered
+		assertEquals( 0, constraintViolations.size() );
+		//end::no-strategy[]
+	}
+
+	@Test
+	public void customNoPrefixStrategyUsed() {
+		//tag::custom-strategy[]
+		Validator validator = Validation.byProvider( HibernateValidator.class )
+				.configure()
+				// Setting a custom getter property selection strategy
+				.getterPropertySelectionStrategy( new NoPrefixGetterPropertySelectionStrategy() )
+				.buildValidatorFactory()
+				.getValidator();
+
+		User user = new User( "first", "last", "not an email" );
+
+		Set<ConstraintViolation<User>> constraintViolations = validator.validate( user );
+
+		assertEquals( 1, constraintViolations.size() );
+		//end::custom-strategy[]
+	}
+}
