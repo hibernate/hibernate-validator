6a240f3a51a60f13054eb8ff68208db19657f2a0
jpoth <poth.john@gmail.com>
2018-06-26 14:28:45 +0200

    HV-1610 make OSGI tests work on JDK 10

---
+++ osgi/integrationtest/src/test/java/org/hibernate/validator/osgi/integrationtest/JavaVersionUtil.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.osgi.integrationtest;
+
+import java.util.Locale;
+
+final class JavaVersionUtil {
+
+	private JavaVersionUtil() {
+	}
+
+	static int getMajorVersion() {
+		String javaSpecVersion = System.getProperty( "java.specification.version" );
+		try {
+			if ( javaSpecVersion.contains( "." ) ) { //before jdk 9
+				return Integer.parseInt( javaSpecVersion.split( "\\." )[1] );
+			}
+			else {
+				return Integer.parseInt( javaSpecVersion );
+			}
+		}
+		catch (NumberFormatException e) {
+			throw getUnableToParseVersionException( javaSpecVersion );
+		}
+	}
+
+	private static IllegalArgumentException getUnableToParseVersionException(String version) {
+		return new IllegalArgumentException( String.format( Locale.ROOT, "We are unable to parse Java version '%1$s'.", version ) );
+	}
+}
