5f275a74bdfc12c1e4d1e680f4768005ed713b07
Yoann Rodière <yoann@hibernate.org>
2019-10-22 09:39:25 +0200

    HV-1755 Test constraints on method parameters of EJB Jax-RS beans

---
+++ integration/src/test/java/org/hibernate/validator/integration/wildfly/ejb/EjbIT.java
@@ -0,0 +1,70 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.integration.wildfly.ejb;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.hibernate.validator.integration.AbstractArquillianIT;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class EjbIT extends AbstractArquillianIT {
+	private static final String WAR_FILE_NAME = EjbIT.class.getSimpleName() + ".war";
+	private static final String APPLICATION_PATH = EjbIT.class.getSimpleName();
+
+	@Deployment
+	public static WebArchive createTestArchive() throws Exception {
+		return buildTestArchive( WAR_FILE_NAME )
+				.addClass( EjbJaxRsResource.class )
+				.addClass( JaxRsApplication.class )
+				.addAsWebInfResource( EmptyAsset.INSTANCE, "beans.xml" )
+				.addAsWebInfResource(
+						new StringAsset(
+								Descriptors.create( org.jboss.shrinkwrap.descriptor.api.webapp31.WebAppDescriptor.class )
+										.exportAsString()
+						),
+						"web.xml"
+				);
+	}
+
+	@Test
+	@RunAsClient
+	public void testRestEasyWorks() {
+		given()
+				.filter( new ErrorLoggingFilter() )
+				.body( "[\"a\", \"b\", \"c\"]" )
+				.contentType( ContentType.JSON )
+				.post( APPLICATION_PATH + "/put/list/" )
+				.then()
+				.statusCode( 200 )
+				.body( equalTo( "Hello bars a, b, c" ) );
+	}
+
+	@Test
+	@RunAsClient
+	public void testValidationWorks() {
+		given()
+				.filter( new ErrorLoggingFilter() )
+				.body( "[]" )
+				.contentType( ContentType.JSON )
+				.post( APPLICATION_PATH + "/put/list/" )
+				.then()
+				.statusCode( 400 )
+				.body( containsString( "must not be empty" ) );
+	}
+}
---
+++ integration/src/test/java/org/hibernate/validator/integration/wildfly/ejb/EjbJaxRsResource.java
@@ -0,0 +1,30 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.integration.wildfly.ejb;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ejb.Stateless;
+import javax.validation.constraints.NotEmpty;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
+public class EjbJaxRsResource {
+
+	@POST
+	@Path("put/list")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String putList(@NotEmpty List<String> a) {
+		return "Hello bars " + a.stream().collect( Collectors.joining( ", " ) );
+	}
+
+}
+
