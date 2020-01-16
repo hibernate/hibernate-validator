d0b73e115dabb6df9c8466ed225b63532e9a5447
Yoann Rodière <yoann@hibernate.org>
2020-01-16 18:12:50 +0100

    HV-1755 Test bean metadata class normalizers provided to the CDI extension

---
+++ cdi/src/test/java/org/hibernate/validator/test/cdi/internal/beanmetadataclassnormalizer/CustomProxyBeanMetaDataClassNormalizer.java
@@ -0,0 +1,89 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.test.cdi.internal.beanmetadataclassnormalizer;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.literal.NamedLiteral;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.hibernate.validator.cdi.spi.BeanNames;
+import org.hibernate.validator.metadata.BeanMetaDataClassNormalizer;
+
+
+public class CustomProxyBeanMetaDataClassNormalizer
+		implements BeanMetaDataClassNormalizer, Bean<BeanMetaDataClassNormalizer> {
+
+	@Override
+	public <T> Class<? super T> normalize(Class<T> clazz) {
+		if ( CustomProxy.class.isAssignableFrom( clazz ) ) {
+			return clazz.getSuperclass();
+		}
+		return clazz;
+	}
+
+	@Override
+	public Class<?> getBeanClass() {
+		return BeanMetaDataClassNormalizer.class;
+	}
+
+	@Override
+	public Set<InjectionPoint> getInjectionPoints() {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public boolean isNullable() {
+		return false;
+	}
+
+	@Override
+	public BeanMetaDataClassNormalizer create(CreationalContext<BeanMetaDataClassNormalizer> creationalContext) {
+		return new CustomProxyBeanMetaDataClassNormalizer();
+	}
+
+	@Override
+	public void destroy(BeanMetaDataClassNormalizer beanMetaDataClassNormalizer,
+			CreationalContext<BeanMetaDataClassNormalizer> creationalContext) {
+		// Nothing to do
+	}
+
+	@Override
+	public Set<Type> getTypes() {
+		return Collections.singleton( BeanMetaDataClassNormalizer.class );
+	}
+
+	@Override
+	public Set<Annotation> getQualifiers() {
+		return Collections.singleton( NamedLiteral.of( BeanNames.BEAN_META_DATA_CLASS_NORMALIZER ) );
+	}
+
+	@Override
+	public Class<? extends Annotation> getScope() {
+		return ApplicationScoped.class;
+	}
+
+	@Override
+	public String getName() {
+		return BeanNames.BEAN_META_DATA_CLASS_NORMALIZER;
+	}
+
+	@Override
+	public Set<Class<? extends Annotation>> getStereotypes() {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public boolean isAlternative() {
+		return false;
+	}
+}
---
+++ cdi/src/test/java/org/hibernate/validator/test/cdi/internal/beanmetadataclassnormalizer/ExtensionProvidedBeanMetadataClassNormalizerTest.java
@@ -0,0 +1,102 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.test.cdi.internal.beanmetadataclassnormalizer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.cdi.HibernateValidator;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+import org.testng.annotations.Test;
+
+public class ExtensionProvidedBeanMetadataClassNormalizerTest extends Arquillian {
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create( JavaArchive.class )
+				.addAsManifestResource( EmptyAsset.INSTANCE, "beans.xml" )
+				// Register the CDI extension that provides the normalizer bean
+				.addAsManifestResource(
+						new StringAsset( CustomProxyBeanMetadataClassNormalizerCdiExtension.class.getName() ),
+						"services/javax.enterprise.inject.spi.Extension"
+				);
+	}
+
+	@HibernateValidator
+	@Inject
+	ValidatorFactory validatorFactory;
+
+	@HibernateValidator
+	@Inject
+	Validator validator;
+
+	@Inject
+	ValidatorFactory defaultValidatorFactory;
+
+	@Inject
+	Validator defaultValidator;
+
+	@Test
+	public void testProxyMetadataIgnoredWithQualifiedValidator() throws Exception {
+		assertThat( validator ).isNotNull();
+		doTest( validator );
+	}
+
+	@Test
+	public void testProxyMetadataIgnoredWithDefaultValidator() throws Exception {
+		assertThat( defaultValidator ).isNotNull();
+		doTest( defaultValidator );
+	}
+
+	@Test
+	public void testProxyMetadataIgnoredWithQualifiedValidatorFactory() throws Exception {
+		assertThat( validatorFactory ).isNotNull();
+		doTest( validatorFactory.getValidator() );
+	}
+
+	@Test
+	public void testProxyMetadataIgnoredWithDefaultValidatorFactory() throws Exception {
+		assertThat( defaultValidatorFactory ).isNotNull();
+		doTest( defaultValidatorFactory.getValidator() );
+	}
+
+	private void doTest(Validator validator) {
+		assertThat( validator ).isNotNull();
+		/*
+		 * Even though we pass an instance of the proxy class that has invalid annotations,
+		 * we expect those to be ignored
+		 * because of the class normalizer we defined.
+		 */
+		assertThat( validator.validate( new TestEntityProxy() ) ).hasSize( 1 );
+	}
+
+	public static class TestEntity {
+		@NotNull
+		private String foo;
+	}
+
+	public static class TestEntityProxy extends TestEntity implements CustomProxy {
+		/*
+		 * This is invalid, but should be ignored because it's defined in a proxy class which gets ignored
+		 * because of the class normalizer we defined.
+		 */
+		@DecimalMax(value = "foo")
+		private String foo;
+	}
+}
