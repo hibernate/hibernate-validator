897aaccfca4a681cc96e323cc474e9ea49b8f217
Yoann Rodière <yoann@hibernate.org>
2020-01-16 17:14:44 +0100

    HV-1755 Allow bean metadata class normalizers to be provided to the CDI extension

---
+++ cdi/src/main/java/org/hibernate/validator/cdi/spi/BeanNames.java
@@ -0,0 +1,16 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.cdi.spi;
+
+public final class BeanNames {
+
+	private BeanNames() {
+	}
+
+	public static final String BEAN_META_DATA_CLASS_NORMALIZER = "hibernate-validator-bean-meta-data-class-normalizer";
+
+}
