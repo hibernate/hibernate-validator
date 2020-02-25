10c27573263ccdd9497acd032fd524c14c35bceb
Yoann Rodière <yoann@hibernate.org>
2020-02-25 15:17:03 +0100

    HV-1774 Add a few tests to demonstrate the behavior of TokenCollector

---
+++ engine/src/test/java/org/hibernate/validator/test/internal/engine/messageinterpolation/TokenCollectorMessageExpressionTest.java
@@ -0,0 +1,110 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.test.internal.engine.messageinterpolation;
+
+import org.hibernate.validator.internal.engine.messageinterpolation.InterpolationTermType;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.MessageDescriptorFormatException;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.Token;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.TokenCollector;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ListAssert;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for {@code TokenCollector} in message expression mode.
+ *
+ * @author Hardy Ferentschik
+ */
+public class TokenCollectorMessageExpressionTest extends AbstractTokenCollectorTest {
+	@Override
+	protected InterpolationTermType getInterpolationTermType() {
+		return InterpolationTermType.EL;
+	}
+
+	// Several tests inherited from the abstract class
+
+	@Test
+	public void testMessageParameter() {
+		ListAssert<Token> assertion = Assertions.assertThat(
+				new TokenCollector( "foo {bar}", getInterpolationTermType() )
+						.getTokenList()
+		)
+				.hasSize( 2 );
+		assertion.element( 0 )
+				.returns( "foo ", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+		assertion.element( 1 )
+				.returns( "{bar}", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+	}
+
+	@Test
+	public void testMessageExpression() {
+		ListAssert<Token> assertion = Assertions.assertThat(
+				new TokenCollector( "foo ${bar}", getInterpolationTermType() )
+						.getTokenList()
+		)
+				.hasSize( 2 );
+		assertion.element( 0 )
+				.returns( "foo ", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+		assertion.element( 1 )
+				.returns( "${bar}", Token::getTokenValue )
+				.returns( true, Token::isParameter );
+	}
+
+	@Test
+	public void testDollarThenDollarThenParameterInterpretedAsLiterals() {
+		ListAssert<Token> assertion = Assertions.assertThat(
+				new TokenCollector( "$${1+1}", getInterpolationTermType() )
+						.getTokenList()
+		)
+				.hasSize( 2 );
+		assertion.element( 0 )
+				.returns( "$$", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+		assertion.element( 1 )
+				.returns( "{1+1}", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+	}
+
+	@Test
+	public void testDollarThenDollarThenLiteralsInterpretedAsLiterals() {
+		ListAssert<Token> assertion = Assertions.assertThat(
+				new TokenCollector( "$$foo", getInterpolationTermType() )
+						.getTokenList()
+		)
+				.hasSize( 2 );
+		assertion.element( 0 )
+				.returns( "$$", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+		assertion.element( 1 )
+				.returns( "foo", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+	}
+
+	@Test(expectedExceptions = MessageDescriptorFormatException.class, expectedExceptionsMessageRegExp = "HV000168.*")
+	public void testDollarThenClosingBraceThrowsException() {
+		new TokenCollector( "$}", getInterpolationTermType() );
+	}
+
+	@Test
+	public void testDollarThenEscapeInterpretedAsLiterals() {
+		ListAssert<Token> assertion = Assertions.assertThat(
+				new TokenCollector( "$\\A{1+1}", getInterpolationTermType() )
+						.getTokenList()
+		)
+				.hasSize( 2 );
+		assertion.element( 0 )
+				.returns( "$\\A", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+		assertion.element( 1 )
+				.returns( "{1+1}", Token::getTokenValue )
+				.returns( false, Token::isParameter );
+	}
+}
