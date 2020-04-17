617c8b12939c9e5cae33d900bf4db90245fcbc6d
Guillaume Smet <guillaume.smet@gmail.com>
2020-04-17 12:58:21 +0200

    HV-1760 Fix javadoc for *OrZero validators

--- engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/bv/number/sign/NegativeOrZeroValidatorForNumber.java
+++ engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/bv/number/sign/NegativeOrZeroValidatorForNumber.java
@@ -11,7 +11,7 @@ import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.constraints.NegativeOrZero;
 
 /**
- * Check that the number being validated is negative.
+ * Check that the number being validated is negative or zero.
  *
  * @author Hardy Ferentschik
  * @author Xavier Sosnovsky
