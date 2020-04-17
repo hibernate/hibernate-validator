8d13e7b3cf78b13d899e347e92a626774bb55dda
Guillaume Smet <guillaume.smet@gmail.com>
2020-04-17 12:55:57 +0200

    HV-1760 Support CharSequence for Negative/Positive/...
    It was documented and it's better for consistency with the Min/Max/...
    constraints.

---
+++ engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/bv/number/sign/PositiveOrZeroValidatorForCharSequence.java
@@ -0,0 +1,36 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.internal.constraintvalidators.bv.number.sign;
+
+import java.math.BigDecimal;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.PositiveOrZero;
+
+/**
+ * Check that the number being validated is positive.
+ *
+ * @author Guillaume Smet
+ */
+public class PositiveOrZeroValidatorForCharSequence implements ConstraintValidator<PositiveOrZero, CharSequence> {
+
+	@Override
+	public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+		// null values are valid
+		if ( value == null ) {
+			return true;
+		}
+
+		try {
+			return NumberSignHelper.signum( new BigDecimal( value.toString() ) ) >= 0;
+		}
+		catch (NumberFormatException nfe) {
+			return false;
+		}
+	}
+}
