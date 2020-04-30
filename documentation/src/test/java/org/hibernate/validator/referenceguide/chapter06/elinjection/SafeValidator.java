37705055aa7bb097ec6203dc5668e03d02cfd552
Guillaume Smet <guillaume.smet@gmail.com>
2020-04-30 16:17:10 +0200

    HV-1773 Be more explicit about issues with EL injection and how to avoid them

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter06/elinjection/SafeValidator.java
@@ -0,0 +1,39 @@
+package org.hibernate.validator.referenceguide.chapter06.elinjection;
+
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+import org.hibernate.validator.referenceguide.chapter06.constraintvalidatorpayload.ZipCode;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+//tag::include[]
+public class SafeValidator implements ConstraintValidator<ZipCode, String> {
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if ( value == null ) {
+			return true;
+		}
+
+		HibernateConstraintValidatorContext hibernateContext = context.unwrap(
+				HibernateConstraintValidatorContext.class );
+		hibernateContext.disableDefaultConstraintViolation();
+
+		if ( isInvalid( value ) ) {
+			hibernateContext
+					.addExpressionVariable( "validatedValue", value )
+					.buildConstraintViolationWithTemplate( "${validatedValue} is not a valid ZIP code" )
+					.addConstraintViolation();
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean isInvalid(String value) {
+		// ...
+		return false;
+	}
+}
+// end::include[]
