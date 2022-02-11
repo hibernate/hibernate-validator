aa6ede67603d5ed447af7af59ee815ec670b28c4
Katalystical <kevin.dargel@onlinehome.de>
2022-02-11 09:01:46 +0100

    HV-1884 Use LinkedHashSet to make composingConstraints order predictable/guaranteed by insertion order
    (cherry picked from commit 44f1f8df74dd6df2c74e2143256e101ea3e3b70c)

--- engine/src/main/java/org/hibernate/validator/internal/metadata/descriptor/ConstraintDescriptorImpl.java
+++ engine/src/main/java/org/hibernate/validator/internal/metadata/descriptor/ConstraintDescriptorImpl.java
@@ -8,6 +8,7 @@ package org.hibernate.validator.internal.metadata.descriptor;
 
 import static org.hibernate.validator.internal.util.CollectionHelper.newHashMap;
 import static org.hibernate.validator.internal.util.CollectionHelper.newHashSet;
+import static org.hibernate.validator.internal.util.CollectionHelper.newLinkedHashSet;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
@@ -614,7 +615,7 @@ public class ConstraintDescriptorImpl<T extends Annotation> implements Constrain
 
 	private Set<ConstraintDescriptorImpl<?>> parseComposingConstraints(ConstraintHelper constraintHelper, Constrainable constrainable,
 			ConstraintType constraintType) {
-		Set<ConstraintDescriptorImpl<?>> composingConstraintsSet = newHashSet();
+		Set<ConstraintDescriptorImpl<?>> composingConstraintsSet = newLinkedHashSet();
 		Map<ClassIndexWrapper, Map<String, Object>> overrideParameters = parseOverrideParameters();
 		Map<Class<? extends Annotation>, ComposingConstraintAnnotationLocation> composingConstraintLocations = new HashMap<>();
 
