2e0373e0010866b269467f8ebd40abf8e68e750b
Jan Schatteman <jschatte@redhat.com>
2022-04-08 18:23:31 +0200

    HV-1863 Add an annotation processor test for records
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

---
+++ annotation-processor/src/test/java17/org/hibernate/validator/ap/record/RecordWithInvalidConstraints.java
@@ -0,0 +1,16 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.ap.record;
+
+import java.util.Date;
+import javax.validation.constraints.FutureOrPresent;
+
+/**
+ * @author Jan Schatteman
+ */
+public record RecordWithInvalidConstraints(/* Not allowed */ @FutureOrPresent String string, @FutureOrPresent Date date) {
+}
---
+++ annotation-processor/src/test/java17/org/hibernate/validator/ap/record/RecordWithInvalidConstructorConstraints.java
@@ -0,0 +1,20 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.ap.record;
+
+import java.util.Date;
+import javax.validation.constraints.FutureOrPresent;
+
+/**
+ * @author Jan Schatteman
+ */
+public record RecordWithInvalidConstructorConstraints(String string, Date date) {
+	public RecordWithInvalidConstructorConstraints(@FutureOrPresent String string, @FutureOrPresent Date date) {
+		this.string = string;
+		this.date = date;
+	}
+}
