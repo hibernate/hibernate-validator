43f60f99c9d476db8a2eafb75e1c268789a9f35f
Jan Schatteman <jschatte@redhat.com>
2022-04-08 18:23:31 +0200

    HV-1863 Add an annotation processor test for records
    Signed-off-by: Jan Schatteman <jschatte@redhat.com>

---
+++ annotation-processor/src/test/java17/org/hibernate/validator/ap/record/RecordConstraintValidationProcessorTest.java
@@ -0,0 +1,91 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.validator.ap.record;
+
+import java.io.File;
+import java.util.EnumSet;
+
+import javax.tools.Diagnostic;
+
+import org.hibernate.validator.ap.ConstraintValidationProcessor;
+import org.hibernate.validator.ap.ConstraintValidationProcessorTestBase;
+import org.hibernate.validator.ap.testutil.CompilerTestHelper;
+import org.hibernate.validator.ap.util.DiagnosticExpectation;
+
+import org.testng.annotations.Test;
+
+import static org.hibernate.validator.ap.testutil.CompilerTestHelper.assertThatDiagnosticsMatch;
+import static org.testng.Assert.assertFalse;
+
+/**
+ * @author Jan Schatteman
+ */
+public class RecordConstraintValidationProcessorTest extends ConstraintValidationProcessorTestBase {
+
+	@Test
+	public void testRecordWithInvalidConstraints() {
+
+		File sourceFile = compilerHelper.getSourceFile( RecordWithInvalidConstraints.class, "/src/test/java17" );
+
+		boolean compilationResult =
+				compilerHelper.compile(
+						new ConstraintValidationProcessor(),
+						diagnostics,
+						EnumSet.of( CompilerTestHelper.Library.VALIDATION_API ),
+						sourceFile
+				);
+
+		assertFalse( compilationResult );
+
+		assertThatDiagnosticsMatch(
+				diagnostics,
+				new DiagnosticExpectation( Diagnostic.Kind.ERROR, 15 )
+		);
+	}
+
+	@Test
+	public void testRecordWithInvalidConstructorConstraints() {
+
+		File sourceFile = compilerHelper.getSourceFile( RecordWithInvalidConstructorConstraints.class, "/src/test/java17" );
+
+		boolean compilationResult =
+				compilerHelper.compile(
+						new ConstraintValidationProcessor(),
+						diagnostics,
+						EnumSet.of( CompilerTestHelper.Library.VALIDATION_API ),
+						sourceFile
+				);
+
+		assertFalse( compilationResult );
+
+		assertThatDiagnosticsMatch(
+				diagnostics,
+				new DiagnosticExpectation( Diagnostic.Kind.ERROR, 16 )
+		);
+	}
+
+	@Test
+	public void testRecordWithInvalidMethodConstraints() {
+
+		File sourceFile = compilerHelper.getSourceFile( RecordWithInvalidMethodConstraints.class, "/src/test/java17" );
+
+		boolean compilationResult =
+				compilerHelper.compile(
+						new ConstraintValidationProcessor(),
+						diagnostics,
+						EnumSet.of( CompilerTestHelper.Library.VALIDATION_API ),
+						sourceFile
+				);
+
+		assertFalse( compilationResult );
+
+		assertThatDiagnosticsMatch(
+				diagnostics,
+				new DiagnosticExpectation( Diagnostic.Kind.ERROR, 19 )
+		);
+	}
+}
