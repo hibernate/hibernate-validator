4a8523cc5609e7e4a3ca133e46e6c9658080ca6a
Guillaume Smet <guillaume.smet@gmail.com>
2022-04-19 18:07:06 +0200

    HV-1863 Adjust annotation processor tests

--- annotation-processor/src/test/java17/org/hibernate/validator/ap/record/RecordWithInvalidMethodConstraints.java
+++ annotation-processor/src/test/java17/org/hibernate/validator/ap/record/RecordWithInvalidMethodConstraints.java
@@ -1,22 +1,21 @@
 /*
- * Hibernate, Relational Persistence for Idiomatic Java
+ * Hibernate Validator, declare and validate application constraints
  *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later
- * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 package org.hibernate.validator.ap.record;
 
 import java.util.Date;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
 
 /**
  * @author Jan Schatteman
  */
 public record RecordWithInvalidMethodConstraints(@NotBlank String string, @FutureOrPresent Date date) {
 
-	public void doNothing(@Positive String s) {
+	public void doNothing(@FutureOrPresent String string) {
 		//
 	}
 }
