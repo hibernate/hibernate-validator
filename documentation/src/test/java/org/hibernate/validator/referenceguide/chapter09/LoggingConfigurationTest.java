ffec81496eff9ae369d6c9fbc0309922d61f93fd
marko-bekhta <marko.prykladna@gmail.com>
2022-06-10 15:01:10 +0200

    HV-1903 Add section to documentation on new configuration

---
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter09/LoggingConfigurationTest.java
@@ -0,0 +1,54 @@
+package org.hibernate.validator.referenceguide.chapter09;
+
+import static org.junit.Assert.assertTrue;
+
+import org.hibernate.validator.HibernateValidator;
+
+import org.junit.Test;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.Positive;
+
+public class LoggingConfigurationTest {
+
+	@Test
+	public void programmaticConfiguration() throws Exception {
+		//tag::programmatic[]
+		Validator validator = Validation.byProvider( HibernateValidator.class )
+				.configure()
+				.showValidatedValuesInTraceLogs( true )
+				.buildValidatorFactory()
+				.getValidator();
+		//end::programmatic[]
+		assertTrue( validator.validate( new Foo( 1 ) ).isEmpty() );
+	}
+
+	@Test
+	public void programmaticConfigurationProperty() throws Exception {
+		//tag::programmaticProperty[]
+		Validator validator = Validation.byProvider( HibernateValidator.class )
+				.configure()
+				.addProperty( "hibernate.validator.show_validated_value_in_trace_logs", "true" )
+				.buildValidatorFactory()
+				.getValidator();
+		//end::programmaticProperty[]
+		assertTrue( validator.validate( new Foo( 1 ) ).isEmpty() );
+	}
+
+
+	public class Foo {
+
+		@Positive
+		private final int value;
+
+		private Foo(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+	}
+
+}
