8dab6f2655627495ac56b437ecf324ba3c475ab8
Yoann Rodière <yoann@hibernate.org>
2023-02-23 13:34:25 +0100

    HV-1939 Test constructor validation on inner classes declared in methods

--- engine/src/test/java/org/hibernate/validator/test/internal/metadata/descriptor/ConstructorDescriptorTest.java
+++ engine/src/test/java/org/hibernate/validator/test/internal/metadata/descriptor/ConstructorDescriptorTest.java
@@ -12,12 +12,15 @@ import static org.hibernate.validator.testutils.ValidatorUtil.getConstructorDesc
 import java.util.List;
 import java.util.Set;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.metadata.ConstraintDescriptor;
 import jakarta.validation.metadata.ConstructorDescriptor;
 import jakarta.validation.metadata.ParameterDescriptor;
 import jakarta.validation.metadata.ReturnValueDescriptor;
 
 import org.hibernate.validator.test.internal.metadata.Customer;
+import org.hibernate.validator.test.internal.metadata.CustomerRepository;
 import org.hibernate.validator.test.internal.metadata.CustomerRepositoryExt;
 import org.hibernate.validator.test.internal.metadata.CustomerRepositoryExt.ValidB2BRepository;
 import org.testng.annotations.Test;
@@ -80,5 +83,44 @@ public class ConstructorDescriptorTest {
 		assertThat( constraintDescriptors ).hasSize( 1 );
 		ConstraintDescriptor<?> descriptor = constraintDescriptors.iterator().next();
 		assertThat( descriptor.getAnnotation().annotationType() ).isEqualTo( ValidB2BRepository.class );
+	}
+
+	@Test
+	public void testMethodInnerClassGetParameterDescriptors() {
+		class MethodInnerClass extends CustomerRepository {
+			public MethodInnerClass(@NotNull String foo, @Valid Customer customer) {
+			}
+		}
+
+		ConstructorDescriptor constructorDescriptor = getConstructorDescriptor(
+				MethodInnerClass.class,
+				ConstructorDescriptorTest.class,
+				String.class,
+				Customer.class
+		);
+
+		List<ParameterDescriptor> parameterDescriptors = constructorDescriptor.getParameterDescriptors();
+		assertThat( parameterDescriptors ).hasSize( 3 );
+
+		// This is the parameter representing the enclosing instance.
+		ParameterDescriptor parameterDescriptor0 = parameterDescriptors.get( 0 );
+		assertThat( parameterDescriptor0.getElementClass() ).isEqualTo( ConstructorDescriptorTest.class );
+		assertThat( parameterDescriptor0.getIndex() ).isEqualTo( 0 );
+		assertThat( parameterDescriptor0.hasConstraints() ).isFalse();
+		assertThat( parameterDescriptor0.isCascaded() ).isFalse();
+
+		ParameterDescriptor parameterDescriptor1 = parameterDescriptors.get( 1 );
+		assertThat( parameterDescriptor1.getElementClass() ).isEqualTo( String.class );
+		assertThat( parameterDescriptor1.getIndex() ).isEqualTo( 1 );
+		assertThat( parameterDescriptor1.getName() ).isEqualTo( "foo" );
+		assertThat( parameterDescriptor1.hasConstraints() ).isTrue();
+		assertThat( parameterDescriptor1.isCascaded() ).isFalse();
+
+		ParameterDescriptor parameterDescriptor2 = parameterDescriptors.get( 2 );
+		assertThat( parameterDescriptor2.getElementClass() ).isEqualTo( Customer.class );
+		assertThat( parameterDescriptor2.getIndex() ).isEqualTo( 2 );
+		assertThat( parameterDescriptor2.getName() ).isEqualTo( "customer" );
+		assertThat( parameterDescriptor2.hasConstraints() ).isFalse();
+		assertThat( parameterDescriptor2.isCascaded() ).isTrue();
 	}
 }
