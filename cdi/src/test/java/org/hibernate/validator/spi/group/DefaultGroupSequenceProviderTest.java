ccd52a4f797b34009ca4263086599b28ea533d57
ilikly <ifinal.io@qq.com>
2023-06-23 12:54:10 +0800

    HV-1942 Add DefaultGroupSequenceProvider test

---
+++ cdi/src/test/java/org/hibernate/validator/spi/group/DefaultGroupSequenceProviderTest.java
@@ -0,0 +1,127 @@
+package org.hibernate.validator.spi.group;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.group.GroupSequenceProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * DefaultGroupSequenceProviderTest.
+ *
+ * @author ilikly
+ */
+public class DefaultGroupSequenceProviderTest {
+
+    @Test
+    public void withoutClassParam() {
+        Assert.assertThrows(() -> {
+            final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+            final Validator validator = validatorFactory.getValidator();
+            validator.validate(new A1());
+        });
+    }
+
+    @Test
+    public void withClassParam() {
+        final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        final Validator validator = validatorFactory.getValidator();
+        validator.validate(new A2());
+    }
+
+
+    public static class DefaultGroupSequenceProvider1 implements DefaultGroupSequenceProvider<Object> {
+
+        @Override
+        public List<Class<?>> getValidationGroups(Object object) {
+            List<Class<?>> groups = new ArrayList<>();
+            if (Objects.nonNull(object)) {
+                groups.add(object.getClass());
+            }
+            return groups;
+        }
+    }
+
+
+    public static class DefaultGroupSequenceProvider2 implements DefaultGroupSequenceProvider<Object> {
+
+        @Override
+        public List<Class<?>> getValidationGroups(Class<?> clazz, Object object) {
+            List<Class<?>> groups = new ArrayList<>();
+            if (Objects.nonNull(clazz)) {
+                groups.add(clazz);
+            }
+            return groups;
+        }
+
+        @Override
+        public List<Class<?>> getValidationGroups(Object object) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+
+    @GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
+    public static class A1 {
+        @NotNull
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
+    public static class B1 {
+        @NotNull
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
+    public static class A2 {
+        @NotNull
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
+    public static class B2 {
+        @NotNull
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
