ace9dec1cafb35f81ed206ef9c288243747092ae
ilikly <ifinal.io@qq.com>
2023-06-23 16:19:20 +0800

    HV-1942 update DefaultGroupSequenceProvider test checkstyle

--- cdi/src/test/java/org/hibernate/validator/spi/group/DefaultGroupSequenceProviderTest.java
+++ cdi/src/test/java/org/hibernate/validator/spi/group/DefaultGroupSequenceProviderTest.java
@@ -26,108 +26,108 @@ import java.util.Objects;
  */
 public class DefaultGroupSequenceProviderTest {
 
-    @Test
-    public void withoutClassParam () {
-        Assert.assertThrows (() -> {
-            final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory ();
-            final Validator validator = validatorFactory.getValidator ();
-            validator.validate (new A1 ());
-        });
-    }
+	@Test
+	public void withoutClassParam () {
+		Assert.assertThrows (() -> {
+			final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory ();
+			final Validator validator = validatorFactory.getValidator ();
+			validator.validate (new A1 ());
+		});
+	}
 
-    @Test
-    public void withClassParam () {
-        final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory ();
-        final Validator validator = validatorFactory.getValidator ();
-        validator.validate (new A2 ());
-    }
-
-
-    public static class DefaultGroupSequenceProvider1 implements DefaultGroupSequenceProvider<Object> {
-
-        @Override
-        public List<Class<?>> getValidationGroups (Object object) {
-            List<Class<?>> groups = new ArrayList<> ();
-            if (Objects.nonNull (object)) {
-                groups.add (object.getClass ());
-            }
-            return groups;
-        }
-    }
+	@Test
+	public void withClassParam () {
+		final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory ();
+		final Validator validator = validatorFactory.getValidator ();
+		validator.validate (new A2 ());
+	}
 
 
-    public static class DefaultGroupSequenceProvider2 implements DefaultGroupSequenceProvider<Object> {
+	public static class DefaultGroupSequenceProvider1 implements DefaultGroupSequenceProvider<Object> {
 
-        @Override
-        public List<Class<?>> getValidationGroups (Class<?> clazz, Object object) {
-            List<Class<?>> groups = new ArrayList<> ();
-            if (Objects.nonNull (clazz)) {
-                groups.add (clazz);
-            }
-            return groups;
-        }
-
-        @Override
-        public List<Class<?>> getValidationGroups (Object object) {
-            throw new IllegalArgumentException ("");
-        }
-    }
+		@Override
+		public List<Class<?>> getValidationGroups (Object object) {
+			List<Class<?>> groups = new ArrayList<> ();
+			if (Objects.nonNull (object)) {
+				groups.add (object.getClass ());
+			}
+			return groups;
+		}
+	}
 
 
-    @GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
-    public static class A1 {
-        @NotNull
-        private String name;
+	public static class DefaultGroupSequenceProvider2 implements DefaultGroupSequenceProvider<Object> {
 
-        public String getName () {
-            return name;
-        }
+		@Override
+		public List<Class<?>> getValidationGroups (Class<?> clazz, Object object) {
+			List<Class<?>> groups = new ArrayList<> ();
+			if (Objects.nonNull (clazz)) {
+				groups.add (clazz);
+			}
+			return groups;
+		}
 
-        public void setName (String name) {
-            this.name = name;
-        }
-    }
+		@Override
+		public List<Class<?>> getValidationGroups (Object object) {
+			throw new IllegalArgumentException ("");
+		}
+	}
 
-    @GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
-    public static class B1 {
-        @NotNull
-        private String name;
 
-        public String getName () {
-            return name;
-        }
+	@GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
+	public static class A1 {
+		@NotNull
+		private String name;
 
-        public void setName (String name) {
-            this.name = name;
-        }
-    }
+		public String getName () {
+			return name;
+		}
 
-    @GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
-    public static class A2 {
-        @NotNull
-        private String name;
+		public void setName (String name) {
+			this.name = name;
+		}
+	}
 
-        public String getName () {
-            return name;
-        }
+	@GroupSequenceProvider(DefaultGroupSequenceProvider1.class)
+	public static class B1 {
+		@NotNull
+		private String name;
 
-        public void setName (String name) {
-            this.name = name;
-        }
-    }
+		public String getName () {
+			return name;
+		}
 
-    @GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
-    public static class B2 {
-        @NotNull
-        private String name;
+		public void setName (String name) {
+			this.name = name;
+		}
+	}
 
-        public String getName () {
-            return name;
-        }
+	@GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
+	public static class A2 {
+		@NotNull
+		private String name;
 
-        public void setName (String name) {
-            this.name = name;
-        }
-    }
+		public String getName () {
+			return name;
+		}
+
+		public void setName (String name) {
+			this.name = name;
+		}
+	}
+
+	@GroupSequenceProvider(DefaultGroupSequenceProvider2.class)
+	public static class B2 {
+		@NotNull
+		private String name;
+
+		public String getName () {
+			return name;
+		}
+
+		public void setName (String name) {
+			this.name = name;
+		}
+	}
 
 }
