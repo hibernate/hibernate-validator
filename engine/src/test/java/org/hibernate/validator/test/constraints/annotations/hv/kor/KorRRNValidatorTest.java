e4d32273654e3011049b88325ca5cb326f847e53
ing9990 <mail80782258@gmail.com>
2024-01-12 10:23:13 +0900

    HV-1970 Adding KorRRN validator tests

---
+++ engine/src/test/java/org/hibernate/validator/test/constraints/annotations/hv/kor/KorRRNValidatorTest.java
@@ -0,0 +1,40 @@
+package org.hibernate.validator.test.constraints.annotations.hv.kor;
+
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertNoViolations;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertThat;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.violationOf;
+
+import jakarta.validation.ConstraintViolation;
+import java.util.Set;
+import org.hibernate.validator.constraints.kor.KorRRN;
+import org.hibernate.validator.test.constraints.annotations.AbstractConstrainedTest;
+import org.testng.annotations.Test;
+
+public class KorRRNValidatorTest extends AbstractConstrainedTest {
+
+    @Test
+    void testKorRRN() {
+        final KorRegistrationCard korRegistrationCard = new KorRegistrationCard("030205-3000000");
+        final Set<ConstraintViolation<KorRegistrationCard>> violations = validator.validate(korRegistrationCard);
+        assertNoViolations(violations);
+    }
+
+    @Test
+    public void testKorRRNInvalid() {
+        final KorRegistrationCard korRegistrationCard = new KorRegistrationCard("000101-5000000");
+        final Set<ConstraintViolation<KorRegistrationCard>> violations = validator.validate(korRegistrationCard);
+        assertThat(violations)
+            .containsOnlyViolations(violationOf(KorRRN.class)
+                .withMessage("invalid Korean resident registration number (KorRRN)"));
+    }
+
+    private static class KorRegistrationCard {
+
+        @KorRRN
+        private final String rrn;
+
+        public KorRegistrationCard(String rrn) {
+            this.rrn = rrn;
+        }
+    }
+}
