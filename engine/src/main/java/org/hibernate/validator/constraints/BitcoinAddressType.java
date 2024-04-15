329599a5dc6c7c140ebdd4394c6f39fdb21e1302
José Yoshiriro <jyoshiriro@gmail.com>
2024-04-15 17:17:39 -0300

    HV-1973 - BitcoinAddressType refactored - pre-compiled pattern

--- engine/src/main/java/org/hibernate/validator/constraints/BitcoinAddressType.java
+++ engine/src/main/java/org/hibernate/validator/constraints/BitcoinAddressType.java
@@ -6,33 +6,27 @@
  */
 package org.hibernate.validator.constraints;
 
+import java.util.regex.Pattern;
+
 public enum BitcoinAddressType {
-	ANY("Bitcoin", null),
-	P2PKH("Legacy (P2PKH)", "^(1)[a-zA-HJ-NP-Z0-9]{25,61}$"),
-	P2SH("Nested SegWit (P2SH)", "^(3)[a-zA-HJ-NP-Z0-9]{33}$"),
-	BECH32("Native SegWit (Bech32)", "^(bc1)[a-zA-HJ-NP-Z0-9]{39,59}$"),
-	P2WSH("SegWit variant of P2SH (P2WSH)", "^(bc1q)[a-zA-HJ-NP-Z0-9]{58}$"),
-	P2WPKH("SegWit variant of P2PKH (P2WPKH)", "^(bc1q)[a-zA-HJ-NP-Z0-9]{38}$"),
-	P2TR("Taproot (P2TR)", "^(bc1p)[a-zA-HJ-NP-Z0-9]{58}$");
+	ANY,
+	P2PKH( "^(1)[a-zA-HJ-NP-Z0-9]{25,61}$"),
+	P2SH( "^(3)[a-zA-HJ-NP-Z0-9]{33}$"),
+	BECH32( "^(bc1)[a-zA-HJ-NP-Z0-9]{39,59}$"),
+	P2WSH( "^(bc1q)[a-zA-HJ-NP-Z0-9]{58}$"),
+	P2WPKH( "^(bc1q)[a-zA-HJ-NP-Z0-9]{38}$"),
+	P2TR("^(bc1p)[a-zA-HJ-NP-Z0-9]{58}$");
 
-	private final String description;
-	private final String regex;
+	private final Pattern pattern;
 
-	BitcoinAddressType(String description, String regex) {
-		this.description = description;
-		this.regex = regex;
+	BitcoinAddressType(String pattern) {
+		this.pattern = pattern != null ? Pattern.compile( pattern ) : null;
+	}
+	BitcoinAddressType() {
+		this( null );
 	}
 
-	public String getDescription() {
-		return description;
-	}
-
-	public String getRegex() {
-		return regex;
-	}
-
-	@Override
-	public String toString() {
-		return this.description;
+	public Pattern getPattern() {
+		return pattern;
 	}
 }
