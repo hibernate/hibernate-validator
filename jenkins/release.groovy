c857404642187834f5d7de45efe9bede172756fa
marko-bekhta <marko.prykladna@gmail.com>
2024-08-01 09:29:20 +0200

    Update hibernate-jenkins-pipeline-helpers

--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -5,7 +5,7 @@
  * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 
-@Library('hibernate-jenkins-pipeline-helpers@1.5') _
+@Library('hibernate-jenkins-pipeline-helpers@1.17') _
 
 import org.hibernate.jenkins.pipeline.helpers.version.Version
 
