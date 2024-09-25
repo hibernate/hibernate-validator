34669b35bd327a1939396fb16dc83839f4cfe7d0
marko-bekhta <marko.prykladna@gmail.com>
2024-09-25 13:24:09 +0200

    HV-2051 Add missing license headers

--- documentation/src/test/java/org/hibernate/validator/referenceguide/chapter09/BootstrappingTest.java
+++ documentation/src/test/java/org/hibernate/validator/referenceguide/chapter09/BootstrappingTest.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
 package org.hibernate.validator.referenceguide.chapter09;
 
 import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertThat;
