b2d02d6b486a0385203667ee99d51281cee0c043
marko-bekhta <marko.prykladna@gmail.com>
2025-06-05 10:47:11 +0200

    Rename copyright.txt to AUTHORS.txt
    to be consistent

--- src/main/scripts/gencopyright.sh
+++ src/main/scripts/gencopyright.sh
@@ -2,11 +2,11 @@
 
 # gencopyright.sh
 #
-# Generates the copyright.txt file mentioned in the license header
+# Generates the AUTHORS.txt file mentioned in the license header
 
 SCRIPT_PATH=$(dirname $0)
 ROOT_PATH="$SCRIPT_PATH/../../../"
-COPYRIGHT_FILE_NAME="copyright.txt"
+COPYRIGHT_FILE_NAME="AUTHORS.txt"
 
 JAVADOC_AUTHORS=$(grep '@author [^<]*' -ho -r --include="*.java" $ROOT_PATH | sed 's/@author//;s/^[[:space:]]*//;s/[[:space:]]*$//;s/"\r"//')
 GIT_AUTHORS=$(git log --pretty=format:"%an")
