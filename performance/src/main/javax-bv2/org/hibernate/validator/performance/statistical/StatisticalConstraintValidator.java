ca8c8aebd042d4bc6b878ccbfe3a88e958312b1a
marko-bekhta <marko.prykladna@gmail.com>
2025-09-17 14:33:42 +0200

    HV-2127 Add a missing SimpleSingleElementValidation/CascadedWithLotsOfItemsAndCyclesValidation for javax benchmarks
    Signed-off-by: marko-bekhta <marko.prykladna@gmail.com>

---
+++ performance/src/main/javax-bv2/org/hibernate/validator/performance/statistical/StatisticalConstraintValidator.java
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.validator.performance.statistical;
+
+import java.lang.annotation.Annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author Hardy Ferentschik
+ */
+public class StatisticalConstraintValidator implements ConstraintValidator<Annotation, Object> {
+	private static final float FAILURE_RATE = 0.25f;
+
+	public static final ThreadLocal<Counter> threadLocalCounter = new ThreadLocal<Counter>() {
+		@Override
+		protected Counter initialValue() {
+			return new Counter();
+		}
+	};
+
+	@Override
+	public void initialize(Annotation constraintAnnotation) {
+	}
+
+	@Override
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
+		return threadLocalCounter.get().incrementCount();
+	}
+
+	public static class Counter {
+		private int totalCount = 0;
+		private int failures = 0;
+
+		public int getFailures() {
+			return failures;
+		}
+
+		public boolean incrementCount() {
+			totalCount++;
+			if ( totalCount * FAILURE_RATE > failures ) {
+				failures++;
+				return false;
+			}
+			return true;
+		}
+
+		public void reset() {
+			totalCount = 0;
+			failures = 0;
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder();
+			sb.append( "Counter" );
+			sb.append( "{totalCount=" ).append( totalCount );
+			sb.append( ", failures=" ).append( failures );
+			sb.append( '}' );
+			return sb.toString();
+		}
+	}
+}
