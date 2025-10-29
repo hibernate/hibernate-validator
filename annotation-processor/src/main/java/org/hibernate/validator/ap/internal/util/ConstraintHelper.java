d90d5bce795431b9e9ae2554608264edc577e21b
marko-bekhta <marko.prykladna@gmail.com>
2025-10-29 08:54:59 +0100

    Account for the fact that validatedBy can now be null in the AP
    since it has a default value of {}
    
    Signed-off-by: marko-bekhta <marko.prykladna@gmail.com>

--- annotation-processor/src/main/java/org/hibernate/validator/ap/internal/util/ConstraintHelper.java
+++ annotation-processor/src/main/java/org/hibernate/validator/ap/internal/util/ConstraintHelper.java
@@ -1014,6 +1014,10 @@ public class ConstraintHelper {
 
 		AnnotationValue validatedBy = annotationApiHelper.getAnnotationValue( constraintMetaAnnotation, "validatedBy" );
 
+		if ( validatedBy == null ) {
+			return List.of();
+		}
+
 		return validatedBy.accept(
 				new SimpleAnnotationValueVisitor8<List<? extends AnnotationValue>, Void>() {
 
