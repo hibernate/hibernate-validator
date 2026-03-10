8d4e3db7343b277c8638a43feac7485f889fe2e2
Koen Aers <koen.aers@gmail.com>
2026-03-10 14:14:59 +0100

    HV-2193: Create a new constraint @NullOrNotBlank similar to @NotBlank for use with nullable fields and Optional
     - Add the initial implementation of constraint NullOrNotBlank
    
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ engine/src/main/java/org/hibernate/validator/constraints/NullOrNotBlank.java
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.validator.constraints;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import org.hibernate.validator.constraints.NullOrNotBlank.List;
+
+/**
+ * The annotated element must be {@code null} or not blank. A character sequence is considered
+ * not blank when it contains at least one non-whitespace character.
+ * <p>
+ * This is particularly useful with {@link java.util.Optional} where an empty {@code Optional}
+ * (extracted as {@code null}) should be valid, but a present value must not be blank.
+ *
+ * @author Koen Aers
+ * @since 9.2
+ */
+@Documented
+@Constraint(validatedBy = { })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Repeatable(List.class)
+public @interface NullOrNotBlank {
+
+	String message() default "{org.hibernate.validator.constraints.NullOrNotBlank.message}";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+
+	/**
+	 * Defines several {@code @NullOrNotBlank} annotations on the same element.
+	 */
+	@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+	@Retention(RUNTIME)
+	@Documented
+	public @interface List {
+		NullOrNotBlank[] value();
+	}
+}
