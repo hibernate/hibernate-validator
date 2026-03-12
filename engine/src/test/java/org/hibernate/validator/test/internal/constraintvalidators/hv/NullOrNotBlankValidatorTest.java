e023b3953dbb64a738e37163cb88b33bcac58673
Koen Aers <koen.aers@gmail.com>
2026-03-12 12:16:15 +0100

    HV-2193: Add @TestForIssue annotation to @NullOrNotBlank test classes
     - Add @TestForIssue(jiraKey = "HV-2193") to NullOrNotBlankValidatorTest and NullOrNotBlankConstrainedTest
     - Add testProgrammaticDefinition test for NullOrNotBlankDef in NullOrNotBlankValidatorTest
    
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- engine/src/test/java/org/hibernate/validator/test/internal/constraintvalidators/hv/NullOrNotBlankValidatorTest.java
+++ engine/src/test/java/org/hibernate/validator/test/internal/constraintvalidators/hv/NullOrNotBlankValidatorTest.java
@@ -4,10 +4,25 @@
  */
 package org.hibernate.validator.test.internal.constraintvalidators.hv;
 
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertNoViolations;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.assertThat;
+import static org.hibernate.validator.testutil.ConstraintViolationAssert.violationOf;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
+import java.util.Set;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.cfg.ConstraintMapping;
+import org.hibernate.validator.cfg.defs.NullOrNotBlankDef;
+import org.hibernate.validator.constraints.NullOrNotBlank;
 import org.hibernate.validator.internal.constraintvalidators.hv.NullOrNotBlankValidator;
+import org.hibernate.validator.testutil.TestForIssue;
+import org.hibernate.validator.testutils.ValidatorUtil;
 
 import org.testng.annotations.Test;
 
@@ -16,6 +31,7 @@ import org.testng.annotations.Test;
  *
  * @author Koen Aers
  */
+@TestForIssue(jiraKey = "HV-2193")
 public class NullOrNotBlankValidatorTest {
 
 	private final NullOrNotBlankValidator validator = new NullOrNotBlankValidator();
@@ -45,4 +61,34 @@ public class NullOrNotBlankValidatorTest {
 		assertFalse( validator.isValid( "   \t\n  ", null ) );
 	}
 
+	@Test
+	public void testProgrammaticDefinition() throws Exception {
+		HibernateValidatorConfiguration config = ValidatorUtil.getConfiguration( HibernateValidator.class );
+		ConstraintMapping mapping = config.createConstraintMapping();
+		mapping.type( Foo.class )
+				.field( "string" )
+				.constraint( new NullOrNotBlankDef() );
+		config.addMapping( mapping );
+		Validator programmaticValidator = config.buildValidatorFactory().getValidator();
+
+		Set<ConstraintViolation<Foo>> violations = programmaticValidator.validate( new Foo( "foobar" ) );
+		assertNoViolations( violations );
+
+		violations = programmaticValidator.validate( new Foo( null ) );
+		assertNoViolations( violations );
+
+		violations = programmaticValidator.validate( new Foo( "   " ) );
+		assertThat( violations ).containsOnlyViolations(
+				violationOf( NullOrNotBlank.class )
+		);
+	}
+
+	private static class Foo {
+
+		private final String string;
+
+		public Foo(String string) {
+			this.string = string;
+		}
+	}
 }
