29968ab1793c930faf53c8e9e1b74e4501fb66cd
marko-bekhta <marko.prykladna@gmail.com>
2026-07-01 17:27:46 +0200

    HV-2235 Add password validation services documentation with real examples

--- engine/src/main/java/org/hibernate/validator/spi/password/CompromisedPasswordResult.java
+++ engine/src/main/java/org/hibernate/validator/spi/password/CompromisedPasswordResult.java
@@ -15,6 +15,14 @@ import org.hibernate.validator.constraintvalidation.HibernateConstraintValidator
 @Incubating
 public interface CompromisedPasswordResult {
 
+	static CompromisedPasswordResult simple(int occurrences) {
+		return new SimpleCompromisedPasswordResult( occurrences > 0, occurrences );
+	}
+
+	static CompromisedPasswordResult simple(boolean compromised, int occurrences) {
+		return new SimpleCompromisedPasswordResult( compromised, occurrences );
+	}
+
 	/**
 	 * @return {@code true} if the password was found in a breach dataset
 	 */
@@ -30,5 +38,8 @@ public interface CompromisedPasswordResult {
 			hibernateConstraintValidatorContext
 					.addMessageParameter( "occurrences", occurrences() );
 		}
+	}
+
+	record SimpleCompromisedPasswordResult(boolean compromised, int occurrences) implements CompromisedPasswordResult {
 	}
 }
