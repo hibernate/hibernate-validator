1054b74e8a1d4dd7f38bdc32c2ec4d157d0de838
marko-bekhta <marko.prykladna@gmail.com>
2026-07-11 21:16:02 +0200

    Switch to a JUnit version of the TCK

--- tck-runner/src/test/java/org/hibernate/validator/tckrunner/securitymanager/TckRunner.java
+++ tck-runner/src/test/java/org/hibernate/validator/tckrunner/securitymanager/TckRunner.java
@@ -4,19 +4,15 @@
  */
 package org.hibernate.validator.tckrunner.securitymanager;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.hibernate.beanvalidation.tck.util.IntegrationTestsMethodSelector;
 import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
-import org.testng.TestNG;
-import org.testng.xml.XmlMethodSelector;
-import org.testng.xml.XmlPackage;
-import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlTest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 /**
  * Main class for running the test suite programmatically, for debugging purposes.
@@ -24,12 +20,8 @@ import org.testng.xml.XmlTest;
  * Specify the following arguments when running this test:
  * <pre>
  * -Dvalidation.provider=org.hibernate.validator.HibernateValidator
- * -Djava.security.manager=default
- * -Djava.security.policy=target/test-classes/test.policy
- * -Djava.security.debug=access
  * -DexcludeIntegrationTests=true
  * -Darquillian.protocol=Local
- * -Dorg.jboss.testharness.spi.StandaloneContainers=org.hibernate.jsr303.tck.util.StandaloneContainersImpl
  * </pre>
  *
  * @author Gunnar Morling
@@ -38,37 +30,25 @@ public class TckRunner {
 
 	@IgnoreForbiddenApisErrors(reason = "Console application that uses System.out and print stack traces.")
 	public static void main(String[] args) {
-		XmlSuite suite = new XmlSuite();
-		suite.setName( "JSR-380-TCK" );
+		LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+				.selectors( DiscoverySelectors.selectPackage( "org.hibernate.beanvalidation.tck.tests" ) )
+				.build();
 
-		XmlTest test = new XmlTest( suite );
-		test.setName( "JSR-380-TCK" );
+		SummaryGeneratingListener listener = new SummaryGeneratingListener();
+		Launcher launcher = LauncherFactory.create();
+		launcher.registerTestExecutionListeners( listener );
+		launcher.execute( request );
 
-		List<XmlPackage> packages = Collections.singletonList( new XmlPackage( "org.hibernate.beanvalidation.tck.tests" ) );
-		test.setXmlPackages( packages );
+		TestExecutionSummary summary = listener.getSummary();
 
-		// Alternatively e.g. use this for running single tests
-		// List<XmlClass> classes = Collections.singletonList( new XmlClass( ValidateTest.class ) );
-		// test.setXmlClasses( classes );
 
-		XmlMethodSelector selector = new XmlMethodSelector();
-		selector.setClassName( IntegrationTestsMethodSelector.class.getName() );
-		test.setMethodSelectors( Collections.singletonList( selector ) );
+		System.out.println( "Tests run: " + summary.getTestsStartedCount()
+				+ ", Failures: " + summary.getTestsFailedCount()
+				+ ", Skipped: " + summary.getTestsSkippedCount() );
 
-		TestListenerAdapter tla = new TestListenerAdapter();
-		TestNG testng = new TestNG();
-		testng.setXmlSuites( Collections.singletonList( suite ) );
-		testng.addListener( tla );
-		testng.run();
-
-		for ( ITestResult failure : tla.getConfigurationFailures() ) {
-			System.out.println( "Failure: " + failure.getName() );
-			failure.getThrowable().printStackTrace();
-		}
-
-		for ( ITestResult result : tla.getFailedTests() ) {
-			System.out.println( "Failed:" + result.getName() );
-			result.getThrowable().printStackTrace();
+		for ( TestExecutionSummary.Failure failure : summary.getFailures() ) {
+			System.out.println( "Failed: " + failure.getTestIdentifier().getDisplayName() );
+			failure.getException().printStackTrace();
 		}
 	}
 }
